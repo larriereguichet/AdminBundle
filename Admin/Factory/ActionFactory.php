0868212d391fe4e359e0781df6c3507eaf4ecb7a
johnkrovitch <arnaudfrezet@gmail.com>
2015-09-17 20:01:07 +0200

    adding filters basic creation, update tests
    updating tests
    
    updating tests

--- Admin/Factory/ActionFactory.php
+++ Admin/Factory/ActionFactory.php
@@ -22,23 +22,29 @@ class ActionFactory
      */
     protected $fieldFactory;
 
-    public function __construct(RoutingLoader $routingLoader, FieldFactory $fieldFactory)
+    /**
+     * @var FilterFactory
+     */
+    protected $filterFactory;
+
+    public function __construct(RoutingLoader $routingLoader, FieldFactory $fieldFactory, FilterFactory $filterFactory)
     {
         $this->routingLoader = $routingLoader;
         $this->fieldFactory = $fieldFactory;
+        $this->filterFactory = $filterFactory;
     }
 
     /**
      * Create an Action from configuration values
      *
-     * @param $actionName
-     * @param $actionConfiguration
+     * @param string $actionName
+     * @param array $actionConfiguration
      * @param Admin $admin
      * @return Action
      */
-    public function create($actionName, $actionConfiguration, Admin $admin)
+    public function create($actionName, array $actionConfiguration, Admin $admin)
     {
-        // resolving default options. Options are different according to action name
+        // resolving default options
         $resolver = new OptionsResolver();
         $resolver->setDefaults($this->getDefaultActionConfiguration());
         $actionConfiguration = $resolver->resolve($actionConfiguration);
@@ -66,6 +72,11 @@ class ActionFactory
             $field = $this->fieldFactory->create($fieldName, $fieldConfiguration);
             $action->addField($field);
         }
+        // adding filters to the action
+        foreach ($actionConfiguration['filters'] as $fieldName => $filterConfiguration) {
+            $filter = $this->filterFactory->create($fieldName, $filterConfiguration);
+            $action->addFilter($filter);
+        }
         return $action;
     }
 
@@ -85,26 +96,11 @@ class ActionFactory
         if ($admin) {
             $action->setRoute($this->routingLoader->generateRouteName($actionName, $admin));
         }
-        return $action;
-    }
-
-    protected function generateDefaultActionTitle($title, $action)
-    {
-        $default = $title;
-
-        if ($action == 'list') {
-            if (substr($title, strlen($title) - 1) == 'y') {
-                $title = substr($title, 0, strlen($title) - 1) . 'ie';
-            }
-            $default = $this->inflectString($title) . 's List';
-        } else if ($action == 'create') {
-            $default = 'Create ' . $this->inflectString($title);
-        } else if ($action == 'edit') {
-            $default = 'Edit ' . $this->inflectString($title);
-        } else if ($action == 'delete') {
-            $default = 'Delete ' . $this->inflectString($title);
+        if (!$action->getTitle()) {
+            $adminName = ($admin) ? $admin->getName() . '.' : '';
+            $action->setTitle(sprintf('bluebear.admin.%s%s', $adminName, $actionName));
         }
-        return $default;
+        return $action;
     }
 
     /**
@@ -121,13 +117,14 @@ class ActionFactory
             ],
             'field_actions' => [],
             'permissions' => ['ROLE_ADMIN'],
-            'export' => ['json', 'xml', 'xls', 'csv', 'html'],
+            'export' => [],
             'order' => [],
             'actions' => [],
             'target' => '_self',
             'route' => '',
             'parameters' => [],
             'icon' => null,
+            'filters' => []
         ];
         return $configuration;
     }
---
+++ Admin/Factory/FilterFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BlueBear\AdminBundle\Admin\Factory;
+
+
+use BlueBear\AdminBundle\Admin\Filter;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FilterFactory
+{
+    /**
+     * @param string $fieldName
+     * @param array $filterConfiguration
+     * @return Filter
+     */
+    public function create($fieldName, array $filterConfiguration)
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setDefaults([
+            'type' => Filter::TYPE_SELECT
+        ]);
+        // setting filters default configuration
+        $resolver->setAllowedValues('type', [
+            Filter::TYPE_SELECT
+        ]);
+        // adding filters to the action
+        $filterConfiguration = $resolver->resolve($filterConfiguration);
+        $filter = new Filter();
+        $filter->setFieldName($fieldName);
+        $filter->setType($filterConfiguration['type']);
+
+        return $filter;
+    }
+}
