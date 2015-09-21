4187ced7e85f70d24d152da5d228528c3df7aa79
johnkrovitch <arnaudfrezet@gmail.com>
2015-09-21 23:11:00 +0200

    improving ActionFactory tests

--- Tests/ActionFactoryFunctionalTest.php
+++ Tests/ActionFactoryFunctionalTest.php
@@ -22,6 +22,7 @@ class ActionFactoryFunctionalTest extends Base
             'actions' => [
                 'minimal_action' => [],
                 'other_action' => [],
+                'full_action' => [],
             ]
         ], new ApplicationConfiguration([], 'en'));
         $fakeAdmin = new Admin('action_test', null, null, $adminConfiguration);
@@ -42,47 +43,95 @@ class ActionFactoryFunctionalTest extends Base
                 'title' => 'MyTitle',
                 'fields' => ['id' => [], 'label' => []],
                 'permissions' => ['MY_ROLE']
+            ],
+            'full_action' => [
+                'title' => 'MyTitle',
+                'fields' => [
+                    'id' => [],
+                    'label' => [
+                        'type' => 'string',
+                        'options' => [
+                            'length' => 50
+                        ]
+                    ],
+                    'my_date' => [
+                        'type' => 'date',
+                        'options' => [
+                            'format' => 'd/m/Y'
+                        ]
+                    ]
+                ],
+                'permissions' => ['MY_ROLE', 'AN_OTHER_ROLE'],
+                'export' => ['json', 'xml'],
+                'order' => [
+                    'test_order' => 'asc'
+                ],
+                'actions' => [
+                    'test' => []
+                ],
+                'field_actions' => [
+                    'test' => []
+                ],
+                'target' => [
+                    '_blank'
+                ],
+                'route' => 'my.route',
+                'parameters' => [
+                    'id' => []
+                ],
+                'icon' => 'an_icon',
+                'filters' => []
             ]
         ];
     }
 
     protected function doTestActionForConfiguration(Action $action, array $configuration, $actionName)
     {
+        $this->assertEquals($actionName, $action->getName());
 
-            $this->assertEquals($actionName, $action->getName());
-
-            if (array_key_exists('title', $configuration)) {
-                // test configured title
-                $this->assertEquals($configuration['title'], $action->getTitle());
-            } else {
-                // test default title
-                $this->assertEquals('bluebear.admin.action_test.' . $actionName, $action->getTitle());
-            }
-            if (array_key_exists('fields', $configuration)) {
-                $this->assertCount(count($configuration['fields']), $action->getFields());
-            }
-            if (array_key_exists('permissions', $configuration)) {
-                $this->assertCount(count($configuration['permissions']), $action->getPermissions());
-                $this->assertEquals($configuration['permissions'], $action->getPermissions());
-            }
-
-
-            /**
-             * 'title' => null,
-            'fields' => [
-            'id' => []
-            ],
-            'field_actions' => [],
-            'permissions' => ['ROLE_ADMIN'],
-            'export' => [],
-            'order' => [],
-            'actions' => [],
-            'target' => '_self',
-            'route' => '',
-            'parameters' => [],
-            'icon' => null,
-            'filters' => []
-             */
-
+        if (array_key_exists('title', $configuration)) {
+            // test configured title
+            $this->assertEquals($configuration['title'], $action->getTitle());
+        } else {
+            // test default title
+            $this->assertEquals('bluebear.admin.action_test.' . $actionName, $action->getTitle());
+        }
+        if (array_key_exists('fields', $configuration)) {
+            // field creation will be tested more in FieldFactory test
+            $this->assertCount(count($configuration['fields']), $action->getFields());
+        }
+        if (array_key_exists('permissions', $configuration)) {
+            $this->assertEquals($configuration['permissions'], $action->getPermissions());
+        }
+        if (array_key_exists('export', $configuration)) {
+            $this->assertEquals($configuration['export'], $action->getExport());
+        }
+        if (array_key_exists('order', $configuration)) {
+            $this->assertCount(count($configuration['order']), $action->getOrder());
+        }
+        if (array_key_exists('actions', $configuration)) {
+            // TODO improve linked action test
+            $this->assertCount(count($configuration['actions']), $action->getActions());
+        }
+        if (array_key_exists('field_actions', $configuration)) {
+            $this->assertCount(count($configuration['field_actions']), $action->getFieldActions());
+        }
+        if (array_key_exists('target', $configuration)) {
+            $this->assertEquals($configuration['target'], $action->getTarget());
+        } else {
+            $this->assertEquals('_self', $action->getTarget());
+        }
+        if (array_key_exists('route', $configuration)) {
+            $this->assertEquals($configuration['route'], $action->getRoute());
+        }
+        if (array_key_exists('parameters', $configuration)) {
+            $this->assertCount(count($configuration['parameters']), $action->getParameters());
+        }
+        if (array_key_exists('icon', $configuration)) {
+            $this->assertEquals($configuration['icon'], $action->getIcon());
+        }
+        if (array_key_exists('filters', $configuration)) {
+            $this->assertCount(count($configuration['filters']), $action->getFilters());
+        }
     }
 }
