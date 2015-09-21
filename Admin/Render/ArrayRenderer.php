15bcdf1fbed569d281ff3674209018cd226edc2c
johnkrovitch <arnaudfrezet@gmail.com>
2015-09-22 00:02:06 +0200

    implements array render

--- Admin/Render/ArrayRenderer.php
+++ Admin/Render/ArrayRenderer.php
@@ -3,17 +3,29 @@
 namespace BlueBear\AdminBundle\Admin\Render;
 
 use BlueBear\AdminBundle\Admin\Configuration\ApplicationConfiguration;
+use Exception;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Traversable;
 
 class ArrayRenderer implements RendererInterface
 {
+    protected $glue;
 
     public function __construct(array $options = [], ApplicationConfiguration $configuration)
     {
-        // TODO: Implement __construct() method.
+        $resolver = new OptionsResolver();
+        $resolver->setDefaults([
+            'glue' => ', '
+        ]);
+        $options = $resolver->resolve($options);
+        $this->glue = $options['glue'];
     }
 
     public function render($value)
     {
-        // TODO: Implement render() method.
+        if (!is_array($value) || $value instanceof Traversable) {
+            throw new Exception('Value should be an array instead of ' . gettype($value));
+        }
+        return implode($this->glue, $value);
     }
 }
