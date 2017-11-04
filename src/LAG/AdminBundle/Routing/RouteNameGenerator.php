3ee65a642515a7505a71b9510f6325c6e346719e
Arnaud Frézet <arnaudfrezet@gmail.com>
2017-11-04 20:05:32 +0100

    add a test for the configuration resolving in the RouteNameGenerator

--- src/LAG/AdminBundle/Routing/RouteNameGenerator.php
+++ src/LAG/AdminBundle/Routing/RouteNameGenerator.php
@@ -7,8 +7,23 @@ use LAG\AdminBundle\Admin\Configuration\AdminConfiguration;
 
 class RouteNameGenerator
 {
+    /**
+     * Generate an admin route name using the pattern in the configuration.
+     *
+     * @param string             $actionName
+     * @param string             $adminName
+     * @param AdminConfiguration $configuration
+     *
+     * @return string
+     *
+     * @throws Exception
+     */
     public function generate($actionName, $adminName, AdminConfiguration $configuration)
     {
+        if (!$configuration->isResolved()) {
+            throw new Exception('The configuration should be resolved before using it');
+        }
+        
         if (!array_key_exists($actionName, $configuration->getParameter('actions'))) {
             throw new Exception(
                 sprintf('Invalid action name %s for admin %s (available action are: %s)',
