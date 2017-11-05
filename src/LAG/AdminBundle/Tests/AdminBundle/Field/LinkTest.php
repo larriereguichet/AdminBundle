c7cfa717988758d140ddcf329c0a8f4733560fd2
Arnaud Frézet <arnaudfrezet@gmail.com>
2017-11-05 13:31:17 +0100

    fix the LinkField test

--- src/LAG/AdminBundle/Tests/AdminBundle/Field/LinkTest.php
+++ src/LAG/AdminBundle/Tests/AdminBundle/Field/LinkTest.php
@@ -4,56 +4,45 @@ namespace LAG\AdminBundle\Tests\AdminBundle\Field;
 
 use LAG\AdminBundle\Field\Field\Link;
 use LAG\AdminBundle\Tests\AdminTestBase;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Translation\IdentityTranslator;
-use Test\TestBundle\Entity\TestEntity;
 use Twig_Environment;
 
 class LinkTest extends AdminTestBase
 {
     public function testRender()
     {
-        $options = [
-            'route' => 'route_test',
-            'parameters' => [
-                'id' => null
-            ],
-            'target' => '_blank',
-            'title' => 'MyTitle',
-            'icon' => 'fa-test',
-            'template' => 'LAGAdminBundle:Render:link.html.twig',
-        ];
-        $resolver = new OptionsResolver();
-    
-        $twig = $this
-            ->getMockBuilder(Twig_Environment::class)
-            ->disableOriginalConstructor()
-            ->getMock()
-        ;
+        $twig = $this->getMockWithoutConstructor(Twig_Environment::class);
         $twig
             ->expects($this->once())
             ->method('render')
-            ->with('LAGAdminBundle:Render:link.html.twig', [
-                'text' => 'test',
-                'route' => 'route_test',
-                'parameters' => $options['parameters'],
-                'target' => '_blank',
-                'url' => '',
-                'title' => 'MyTitle',
-                'icon' => 'fa-test',
+            ->with('link.html.twig', [
+                'text' => 'a link text',
+                'parameters' => [],
+                'options' => [
+                    'text' => null,
+                    'translation' => null,
+                    'length' => null,
+                    'replace' => null,
+                    'parameters' => [],
+                    'template' => 'link.html.twig',
+                ],
             ])
-            ->willReturn('<p>lol man</p>')
+            ->willReturn('html content')
         ;
         
-        $linkField = new Link();
-        $linkField->setApplicationConfiguration($this->createApplicationConfiguration());
-        $linkField->configureOptions($resolver);
-        $linkField->setOptions($resolver->resolve($options));
-        $linkField->setTranslator(new IdentityTranslator());
+        $linkField = new Link('my-field');
         $linkField->setTwig($twig);
-        $linkField->setEntity(new TestEntity());
-
-        $result = $linkField->render('test');
-        $this->assertInternalType('string', $result);
+    
+        $this->setPrivateProperty($linkField, 'options', [
+            'text' => null,
+            'translation' => null,
+            'length' => null,
+            'replace' => null,
+            'parameters' => [],
+            'template' => 'link.html.twig',
+        ]);
+        
+        $content = $linkField->render('a link text');
+    
+        $this->assertEquals('html content', $content);
     }
 }
