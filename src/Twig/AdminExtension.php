ae58c2372b158d04151a6f63127eb4b88f50113e
Arnaud Frézet <arnaudfrezet@gmail.com>
2018-03-05 20:13:07 +0100

    add fields function in the Twig extension

--- src/Twig/AdminExtension.php
+++ src/Twig/AdminExtension.php
@@ -5,13 +5,22 @@ namespace LAG\AdminBundle\Twig;
 use LAG\AdminBundle\Configuration\ApplicationConfiguration;
 use LAG\AdminBundle\Configuration\ApplicationConfigurationStorage;
 use LAG\AdminBundle\Configuration\MenuItemConfiguration;
+use LAG\AdminBundle\Exception\Exception;
 use LAG\AdminBundle\Factory\MenuFactory;
+use LAG\AdminBundle\Field\EntityAwareFieldInterface;
+use LAG\AdminBundle\Field\FieldInterface;
+use LAG\AdminBundle\Utils\StringUtilTrait;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+use Twig_Environment;
 use Twig_Extension;
 use Twig_SimpleFunction;
 
 class AdminExtension extends Twig_Extension
 {
+    use StringUtilTrait;
+
     /**
      * @var ApplicationConfiguration
      */
@@ -23,7 +32,7 @@ class AdminExtension extends Twig_Extension
     private $menuFactory;
 
     /**
-     * @var \Twig_Environment
+     * @var Twig_Environment
      */
     private $twig;
 
@@ -32,16 +41,32 @@ class AdminExtension extends Twig_Extension
      */
     private $router;
 
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * AdminExtension constructor.
+     *
+     * @param ApplicationConfigurationStorage $applicationConfigurationStorage
+     * @param MenuFactory                     $menuFactory
+     * @param Twig_Environment               $twig
+     * @param RouterInterface                 $router
+     * @param TranslatorInterface             $translator
+     */
     public function __construct(
         ApplicationConfigurationStorage $applicationConfigurationStorage,
         MenuFactory $menuFactory,
-        \Twig_Environment $twig,
-        RouterInterface $router
+        Twig_Environment $twig,
+        RouterInterface $router,
+        TranslatorInterface $translator
     ) {
         $this->applicationConfiguration = $applicationConfigurationStorage->getConfiguration();
         $this->menuFactory = $menuFactory;
         $this->twig = $twig;
         $this->router = $router;
+        $this->translator = $translator;
     }
 
     public function getFunctions()
@@ -50,6 +75,8 @@ class AdminExtension extends Twig_Extension
             new Twig_SimpleFunction('admin_config', [$this, 'getApplicationParameter']),
             new Twig_SimpleFunction('admin_menu', [$this, 'getMenu']),
             new Twig_SimpleFunction('admin_menu_action', [$this, 'getMenuAction']),
+            new Twig_SimpleFunction('admin_field_header', [$this, 'getFieldHeader']),
+            new Twig_SimpleFunction('admin_field', [$this, 'getField']),
         ];
     }
 
@@ -90,5 +117,50 @@ class AdminExtension extends Twig_Extension
         }
 
         return $this->router->generate($configuration->getParameter('route'));
+    }
+
+    public function getFieldHeader(FieldInterface $field)
+    {
+        if ($this->applicationConfiguration->getParameter('enable_translation')) {
+            throw new Exception('Translation is not implemented yet');
+        } else {
+            $title = $this->camelize($field->getName());
+            $title = preg_replace('/(?<!\ )[A-Z]/', ' $0', $title);
+            $title = trim($title);
+
+            if ('Id' === $title) {
+                $title = '#';
+            }
+        }
+
+        return $title;
+    }
+
+    /**
+     * Render a field of an entity.
+     *
+     * @param FieldInterface $field
+     * @param $entity
+     *
+     * @return string
+     */
+    public function getField(FieldInterface $field, $entity)
+    {
+        $value = null;
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        // if name starts with a underscore, it is a custom field, not mapped to the entity
+        if (substr($field->getName(), 0, 1) != '_') {
+            // get raw value from object
+            $value = $accessor->getValue($entity, $field->getName());
+        }
+
+        if ($field instanceof EntityAwareFieldInterface) {
+            $field->setEntity($entity);
+        }
+
+        $render = $field->render($value);
+
+        return $render;
     }
 }
