d778a67244be7339524f0a15b06e83787600a7df
Arnaud Frézet <arnaudfrezet@gmail.com>
2018-03-05 20:03:14 +0100

    refactor the doctrine orm provider

--- src/DataProvider/DataProviderInterface.php
+++ src/DataProvider/DataProviderInterface.php
@@ -7,56 +7,5 @@ namespace LAG\AdminBundle\DataProvider;
  */
 interface DataProviderInterface
 {
-    /**
-     * Save an entity.
-     *
-     * @param $entity
-     */
-    public function save($entity);
-
-    /**
-     * Remove an entity.
-     *
-     * @param $entity
-     */
-    public function remove($entity);
-
-    /**
-     * Create an new entity.
-     *
-     * @return mixed
-     */
-    public function create();
-    
-    /**
-     * Find entities according to the given criteria.
-     *
-     * @param array $criteria
-     * @param array $orderBy
-     * @param null|int  $limit
-     * @param null|int  $offset
-     *
-     * @return mixed
-     */
-    public function findBy(array $criteria = [], $orderBy = [], $limit = null, $offset = null);
-
-    /**
-     * Find an entity according to its unique id.
-     *
-     * @param $id
-     *
-     * @return mixed
-     */
-    public function find($id);
-    
-    /**
-     * Return the total number of entities managed by the DataProvider.
-     *
-     * @param array $criteria
-     *
-     * @param array $options
-     *
-     * @return int
-     */
-    public function count(array $criteria = [], array $options = []);
+    public function getCollection(string $entityClass);
 }
--- src/DataProvider/DoctrineOrmDataProvider.php
+++ src/DataProvider/DoctrineOrmDataProvider.php
@@ -13,96 +13,23 @@ class DoctrineOrmDataProvider implements DataProviderInterface
     private $entityManager;
 
     /**
-     * @var string
-     */
-    private $entityClass;
-
-    /**
-     * @var EntityRepository
-     */
-    private $repository;
-
-    /**
      * DoctrineOrmDataProvider constructor.
      *
      * @param EntityManagerInterface $entityManager
-     * @param string                 $entityClass
      */
-    public function __construct(EntityManagerInterface $entityManager, $entityClass)
+    public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
-        $this->entityClass = $entityClass;
-        $this->repository = $entityManager->getRepository($entityClass);
     }
 
-    /**
-     * Save an entity.
-     *
-     * @param $entity
-     */
-    public function save($entity)
+    public function getCollection(string $entityClass)
     {
-        // TODO: Implement save() method.
-    }
+        /** @var EntityRepository $repository */
+        $repository = $this->entityManager->getRepository($entityClass);
+        $queryBuilder = $repository->createQueryBuilder('entity');
 
-    /**
-     * Remove an entity.
-     *
-     * @param $entity
-     */
-    public function remove($entity)
-    {
-        // TODO: Implement remove() method.
-    }
+        $entities = $queryBuilder->getQuery()->getResult();
 
-    /**
-     * Create an new entity.
-     *
-     * @return mixed
-     */
-    public function create()
-    {
-        // TODO: Implement create() method.
-    }
-
-    /**
-     * Find entities according to the given criteria.
-     *
-     * @param array    $criteria
-     * @param array    $orderBy
-     * @param null|int $limit
-     * @param null|int $offset
-     *
-     * @return mixed
-     */
-    public function findBy(array $criteria = [], $orderBy = [], $limit = null, $offset = null)
-    {
-        // TODO: Implement findBy() method.
-    }
-
-    /**
-     * Find an entity according to its unique id.
-     *
-     * @param $id
-     *
-     * @return mixed
-     */
-    public function find($id)
-    {
-        // TODO: Implement find() method.
-    }
-
-    /**
-     * Return the total number of entities managed by the DataProvider.
-     *
-     * @param array $criteria
-     *
-     * @param array $options
-     *
-     * @return int
-     */
-    public function count(array $criteria = [], array $options = [])
-    {
-        // TODO: Implement count() method.
+        return $entities;
     }
 }
