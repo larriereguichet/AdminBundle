eaacacb5450b3f9d54bae2f9f95f5c8f1e2b4e23
Arnaud Frézet <arnaudfrezet@gmail.com>
2018-04-20 20:20:55 +0200

    Fix the ApplicationConfigurationCompilerPass with the righteous configuration class

--- src/DependencyInjection/CompilerPass/ApplicationConfigurationCompilerPass.php
+++ src/DependencyInjection/CompilerPass/ApplicationConfigurationCompilerPass.php
@@ -2,7 +2,7 @@
 
 namespace LAG\AdminBundle\DependencyInjection\CompilerPass;
 
-use LAG\AdminBundle\Configuration\AdminConfiguration;
+use LAG\AdminBundle\Configuration\ApplicationConfigurationStorage;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -10,7 +10,7 @@ class ApplicationConfigurationCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        $definition = $container->getDefinition(AdminConfiguration::class);
+        $definition = $container->getDefinition(ApplicationConfigurationStorage::class);
         $configuration = $container->getParameter('lag.admin.application_configuration');
 
         $definition->addMethodCall('setConfiguration', [
