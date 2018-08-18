48a47f8e20299bae908b2f20b0b713eac05acadf
Arnaud Frézet <arnaudfrezet@gmail.com>
2018-08-18 20:35:25 +0200

    Improve the mapped field by rendering translated strings instead of raw strings

--- src/Field/MappedField.php
+++ src/Field/MappedField.php
@@ -3,13 +3,18 @@
 namespace LAG\AdminBundle\Field;
 
 use LAG\AdminBundle\Configuration\ActionConfiguration;
+use LAG\AdminBundle\Field\Traits\TranslatorTrait;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MappedField extends AbstractField
+class MappedField extends AbstractField implements TranslatorAwareFieldInterface
 {
+    use TranslatorTrait;
+
+    private $translationEnabled = false;
+
     public function isSortable(): bool
     {
-        return false;
+        return true;
     }
 
     public function configureOptions(OptionsResolver $resolver, ActionConfiguration $actionConfiguration)
@@ -18,12 +23,17 @@ class MappedField extends AbstractField
             ->setRequired('map')
             ->setAllowedTypes('map', 'array')
         ;
+        $this->translationEnabled = $actionConfiguration->getAdminConfiguration()->getParameter('translation_enabled');
     }
 
     public function render($value = null): string
     {
         if (key_exists($value, $this->options['map'])) {
             $value = $this->options['map'][$value];
+        }
+
+        if ($this->translationEnabled) {
+            $value = $this->translator->trans($value);
         }
 
         return (string)$value;
---
+++ src/Field/Traits/TranslatorTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace LAG\AdminBundle\Field\Traits;
+
+use Symfony\Component\Translation\TranslatorInterface;
+
+trait TranslatorTrait
+{
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @return TranslatorInterface
+     */
+    public function getTranslator(): TranslatorInterface
+    {
+        return $this->translator;
+    }
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function setTranslator(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+}
