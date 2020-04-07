a7808c2991d84545fa3612b40bf82f189c59a728
johnkrovitch <arnaudfrezet@gmail.com>
2020-04-07 18:13:15 +0200

    Use the real path to load yaml resources

--- src/Resource/Loader/ResourceLoader.php
+++ src/Resource/Loader/ResourceLoader.php
@@ -19,6 +19,7 @@ class ResourceLoader
     public function load(string $resourcesPath): array
     {
         $fileSystem = new Filesystem();
+        $resourcesPath = realpath($resourcesPath);
 
         if (!$fileSystem->exists($resourcesPath)) {
             throw new FileNotFoundException(null, 0, null, $resourcesPath);
