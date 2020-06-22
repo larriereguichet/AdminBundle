42ab0e8176534e7e1a64ca092bf9642512128c5c
johnkrovitch <arnaudfrezet@gmail.com>
2020-06-22 20:49:51 +0200

    Improve AdminDataCollector by not trying to display data when an exception occurred during the configuration

--- src/Debug/DataCollector/AdminDataCollector.php
+++ src/Debug/DataCollector/AdminDataCollector.php
@@ -3,8 +3,9 @@
 namespace LAG\AdminBundle\Debug\DataCollector;
 
 use Exception;
+use LAG\AdminBundle\Bridge\KnpMenu\Provider\MenuProvider;
 use LAG\AdminBundle\Configuration\ApplicationConfigurationStorage;
-use LAG\AdminBundle\Menu\Provider\MenuProvider;
+use LAG\AdminBundle\Exception\ConfigurationException;
 use LAG\AdminBundle\Resource\Registry\ResourceRegistryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -48,29 +49,38 @@ class AdminDataCollector extends DataCollector
             'menus' => [],
         ];
 
-        foreach ($this->registry->all() as $resource) {
-            $data['admins'][$resource->getName()] = [
-                'entity_class' => $resource->getEntityClass(),
-                'configuration' => $resource->getConfiguration(),
-            ];
-        }
-
-        foreach ($this->storage->getConfiguration()->all() as $name => $parameter) {
-            $data['application'][$name] = $parameter;
-        }
-
-        foreach ($this->menuProvider->all() as $menuName => $menu) {
-            $data['menus'][$menuName] = [
-                'attributes' => $menu->getAttributes(),
-                'displayed' => $menu->isDisplayed(),
-            ];
-
-            foreach ($menu->getChildren() as $childName => $child) {
-                $data['menus'][$menuName]['children'][$childName] = [
-                    'attributes' => $child->getAttributes(),
-                    'displayed' => $child->isDisplayed(),
-                    'uri' => $child->getUri(),
+        if ($exception instanceof ConfigurationException) {
+            $data['error'] = 'An error has occurred during the configuration resolving. Data can not displayed';
+            $data['configuration'] = $exception->getConfiguration();
+        } else {
+            foreach ($this->registry->all() as $resource) {
+                $data['admins'][$resource->getName()] = [
+                    'entity_class' => $resource->getEntityClass(),
+                    'configuration' => $resource->getConfiguration(),
                 ];
+            }
+
+            // When the application configuration is not defined or resolved, we can not access to the admin/menus
+            // configuration
+            if ($this->storage->isFrozen()) {
+                foreach ($this->storage->getConfiguration()->all() as $name => $parameter) {
+                    $data['application'][$name] = $parameter;
+                }
+
+                foreach ($this->menuProvider->all() as $menuName => $menu) {
+                    $data['menus'][$menuName] = [
+                        'attributes' => $menu->getAttributes(),
+                        'displayed' => $menu->isDisplayed(),
+                    ];
+
+                    foreach ($menu->getChildren() as $childName => $child) {
+                        $data['menus'][$menuName]['children'][$childName] = [
+                            'attributes' => $child->getAttributes(),
+                            'displayed' => $child->isDisplayed(),
+                            'uri' => $child->getUri(),
+                        ];
+                    }
+                }
             }
         }
 
