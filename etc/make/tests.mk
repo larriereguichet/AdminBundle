d4412e6ffd85360d0211227651fff26c77e39d1f
johnkrovitch <arnaudfrezet@gmail.com>
2020-10-22 22:04:40 +0200

    Add a check for dump in the tests suite

--- etc/make/tests.mk
+++ etc/make/tests.mk
@@ -1,9 +1,9 @@
 .PHONY: tests tests.ci tests.stop-on-failure php-cs-fixer.fix php-cs-fixer.ci phpstan.analyse phpunit.run security.check tests.stop-on-failure bc.check
 
 # PHPUnit
-tests: phpunit.run php-cs-fixer.fix phpstan.analyse security.check
+tests: phpunit.run php-cs-fixer.fix phpstan.analyse security.check tests.var-dump-checker
 
-tests.ci: phpunit.run php-cs-fixer.ci phpstan.analyse security.check
+tests.ci: phpunit.run php-cs-fixer.ci phpstan.analyse security.check tests.var-dump-checker
 
 tests.stop-on-failure:
 	bin/phpunit --stop-on-failure -v
@@ -39,3 +39,6 @@ security.check:
 
 bc.check:
 	bin/roave-backward-compatibility-check
+
+tests.var-dump-checker:
+	bin/var-dump-check --symfony --exclude vendor .
