35e2299b23d419898b8d16fdf1cf0b555b9f3552
johnkrovitch <arnaudfrezet@gmail.com>
2021-04-11 15:30:48 +0200

    Add comments on public interfaces

--- src/Admin/AdminAwareInterface.php
+++ src/Admin/AdminAwareInterface.php
@@ -4,5 +4,10 @@ namespace LAG\AdminBundle\Admin;
 
 interface AdminAwareInterface
 {
+    /**
+     * Define the current admin.
+     *
+     * @param AdminInterface $admin
+     */
     public function setAdmin(AdminInterface $admin): void;
 }
--- src/Admin/Helper/AdminHelperInterface.php
+++ src/Admin/Helper/AdminHelperInterface.php
@@ -3,12 +3,30 @@
 namespace LAG\AdminBundle\Admin\Helper;
 
 use LAG\AdminBundle\Admin\AdminInterface;
+use LAG\AdminBundle\Exception\Exception;
 
 interface AdminHelperInterface
 {
+    /**
+     * Define the current admin based on the request parameters.
+     *
+     * @param AdminInterface $admin
+     */
     public function setAdmin(AdminInterface $admin): void;
 
+    /**
+     * Returns the current admin.
+     *
+     * @return AdminInterface
+     *
+     * @throws Exception An exception is thrown if no admin has been defined
+     */
     public function getAdmin(): AdminInterface;
 
+    /**
+     * Return true if the current admin has been defined.
+     *
+     * @return bool
+     */
     public function hasAdmin(): bool;
 }
