78e9e86e1885a04394582063a28593d251ef79bd
Arnaud Frézet <arnaudfrezet@gmail.com>
2021-05-13 20:38:42 +0200

    Move the menus security logic into the factory to be more coherent and to avoid resolving the configuration twice

--- src/Menu/Factory/MenuFactory.php
+++ src/Menu/Factory/MenuFactory.php
@@ -9,6 +9,7 @@ use Knp\Menu\ItemInterface;
 use LAG\AdminBundle\Event\Events\MenuEvent;
 use LAG\AdminBundle\Event\MenuEvents;
 use LAG\AdminBundle\Factory\Configuration\ConfigurationFactoryInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class MenuFactory implements MenuFactoryInterface
@@ -17,30 +18,43 @@ class MenuFactory implements MenuFactoryInterface
     private FactoryInterface $factory;
     private ConfigurationFactoryInterface $configurationFactory;
     private MenuItemFactoryInterface $menuItemFactory;
+    private Security $security;
 
     public function __construct(
         EventDispatcherInterface $eventDispatcher,
         FactoryInterface $factory,
         ConfigurationFactoryInterface $configurationFactory,
-        MenuItemFactoryInterface $menuItemFactory
+        MenuItemFactoryInterface $menuItemFactory,
+        Security $security
     ) {
         $this->eventDispatcher = $eventDispatcher;
         $this->factory = $factory;
         $this->configurationFactory = $configurationFactory;
         $this->menuItemFactory = $menuItemFactory;
+        $this->security = $security;
     }
 
     public function create(string $name, array $options = []): ItemInterface
     {
-        $menuConfiguration = $this->configurationFactory->createMenuConfiguration($name, $options)->toArray();
+        $menuConfiguration = $this->configurationFactory->createMenuConfiguration($name, $options);
         $menu = $this->factory->createItem('root', [
-            'attributes' => $menuConfiguration['attributes'],
-            'extras' => $menuConfiguration['extras'],
+            'attributes' => $menuConfiguration->get('attributes'),
+            'extras' => $menuConfiguration->get('extras'),
         ]);
+
+        if (!$menuConfiguration->hasPermissions()) {
+            return $menu;
+        }
+
+        foreach ($menuConfiguration->getPermissions() as $permission) {
+            if (!$this->security->isGranted($permission, $this->security->getUser())) {
+                return $menu;
+            }
+        }
         $event = new MenuEvent($name, $menu);
         $this->eventDispatcher->dispatch($event, MenuEvents::MENU_CREATE);
         $this->eventDispatcher->dispatch($event, sprintf(MenuEvents::MENU_CREATE_SPECIFIC, $name));
-        $children = $menuConfiguration['children'] ?? [];
+        $children = $menuConfiguration->get('children') ?? [];
 
         foreach ($children as $child) {
             $child = $this->menuItemFactory->create($child['text'], $child);
--- src/Menu/Provider/MenuProvider.php
+++ src/Menu/Provider/MenuProvider.php
@@ -6,9 +6,7 @@ namespace LAG\AdminBundle\Menu\Provider;
 
 use Knp\Menu\ItemInterface;
 use Knp\Menu\Provider\MenuProviderInterface;
-use LAG\AdminBundle\Factory\Configuration\ConfigurationFactoryInterface;
 use LAG\AdminBundle\Menu\Factory\MenuFactoryInterface;
-use Symfony\Component\Security\Core\Security;
 
 /**
  * Create a new KNP menu using the MenuConfiguration class to validate provided options.
@@ -17,18 +15,12 @@ class MenuProvider implements MenuProviderInterface
 {
     private array $menuConfigurations;
     private MenuFactoryInterface $menuFactory;
-    private ConfigurationFactoryInterface $configurationFactory;
-    private Security $security;
 
     public function __construct(
         array $menuConfigurations,
-        MenuFactoryInterface $menuFactory,
-        ConfigurationFactoryInterface $configurationFactory,
-        Security $security
+        MenuFactoryInterface $menuFactory
     ) {
-        $this->configurationFactory = $configurationFactory;
         $this->menuConfigurations = $menuConfigurations;
-        $this->security = $security;
         $this->menuFactory = $menuFactory;
     }
 
@@ -39,21 +31,6 @@ class MenuProvider implements MenuProviderInterface
 
     public function has(string $name, array $options = []): bool
     {
-        if (!\array_key_exists($name, $this->menuConfigurations)) {
-            return false;
-        }
-        $configuration = $this->configurationFactory->createMenuConfiguration($name, $options);
-
-        if (!$configuration->hasPermissions()) {
-            return true;
-        }
-
-        foreach ($configuration->getPermissions() as $permission) {
-            if (!$this->security->isGranted($permission, $this->security->getUser())) {
-                return false;
-            }
-        }
-
-        return true;
+        return array_key_exists($name, $this->menuConfigurations);
     }
 }
