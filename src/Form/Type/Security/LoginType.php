ffa2e43dabfb70bde8bd178bd43ac1c3221f98ab
Arnaud Frézet <arnaudfrezet@gmail.com>
2022-02-07 20:01:24 +0100

    feature: change the way the translation works to be easier to use

--- src/Form/Type/Security/LoginType.php
+++ src/Form/Type/Security/LoginType.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\Form\Type\Security;
 
+use LAG\AdminBundle\Translation\Helper\TranslationHelperInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -11,23 +12,30 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class LoginType extends AbstractType
 {
+    public function __construct(private TranslationHelperInterface $translationHelper)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('_login', TextType::class, [
+            ->add('_username', TextType::class, [
                 'attr' => ['class' => 'form-control-user'],
-                'label' => 'lag.admin.login_form.login',
+                'label' => $this->translationHelper->getTranslationKey('login_form.login'),
+                'translation_domain' => $this->translationHelper->getTranslationDomain(),
                 'block_prefix' => '_',
             ])
             ->add('_password', PasswordType::class, [
                 'attr' => ['class' => 'form-control-user'],
-                'label' => 'lag.admin.login_form.password',
+                'label' => $this->translationHelper->getTranslationKey('login_form.password'),
+                'translation_domain' => $this->translationHelper->getTranslationDomain(),
             ])
         ;
     }
 
     public function getBlockPrefix(): string
     {
+        // No login prefix as the login form is read directly by the Symfony security
         return '';
     }
 }
