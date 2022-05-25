88219ea1b1e871b71d571a5da6f617cea1fad3ea
Arnaud Frézet <arnaudfrezet@gmail.com>
2022-05-25 19:21:01 +0200

    move docker php 8.1

---
+++ tests/phpunit/Action/ActionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LAG\AdminBundle\Tests\Action;
+
+use LAG\AdminBundle\Action\Action;
+use LAG\AdminBundle\Action\Configuration\ActionConfiguration;
+use LAG\AdminBundle\Tests\TestCase;
+
+class ActionTest extends TestCase
+{
+    public function testGetters(): void
+    {
+        $configuration = $this->createMock(ActionConfiguration::class);
+        $action = new Action('test', $configuration);
+
+        $this->assertEquals('test', $action->getName());
+        $this->assertEquals($configuration, $action->getConfiguration());
+    }
+}
