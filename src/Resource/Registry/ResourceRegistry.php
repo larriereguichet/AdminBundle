7a42d7563524e198e0d30ea19b0322cf89181042
Arnaud Frézet <arnaudfrezet@gmail.com>
2022-10-04 18:53:04 +0200

    feature: add a new resource registry

--- src/Resource/Registry/ResourceRegistry.php
+++ src/Resource/Registry/ResourceRegistry.php
@@ -4,25 +4,45 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\Resource\Registry;
 
+use LAG\AdminBundle\Admin\Factory\AdminFactoryInterface;
 use LAG\AdminBundle\Exception\Exception;
-use LAG\AdminBundle\Resource\AdminResource;
-use LAG\AdminBundle\Resource\Locator\ResourceLocatorInterface;
+use LAG\AdminBundle\Exception\UnexpectedTypeException;
+use LAG\AdminBundle\Metadata\Admin;
+use LAG\AdminBundle\Metadata\Locator\MetadataLocatorInterface;
 
 class ResourceRegistry implements ResourceRegistryInterface
 {
     private array $resources = [];
+    private bool $loaded = false;
 
     public function __construct(
-        array $resourcesPath,
-        ResourceLocatorInterface $locator,
+        private array $resourcePaths,
+        private MetadataLocatorInterface $locator,
+        private AdminFactoryInterface $adminFactory,
     ) {
-        foreach ($resourcesPath as $path) {
-            $resources = $locator->locate($path);
+    }
+
+    public function load(): void
+    {
+        if ($this->loaded) {
+            return;
+        }
+
+        foreach ($this->resourcePaths as $path) {
+            $resources = $this->locator->locateCollection($path);
 
             foreach ($resources as $resource) {
-                $this->resources[$resource->getName()] = $resource;
+                if (!$resource instanceof Admin) {
+                    throw new UnexpectedTypeException($resource, Admin::class);
+                }
+
+                if (!$resource->getName()) {
+                    throw new Exception('The admin resource has no name');
+                }
+                $this->resources[$resource->getName()] = $this->adminFactory->create($resource);
             }
         }
+        $this->loaded = true;
     }
 
     public function has(string $resourceName): bool
@@ -30,7 +50,7 @@ class ResourceRegistry implements ResourceRegistryInterface
         return \array_key_exists($resourceName, $this->resources);
     }
 
-    public function get(string $resourceName): AdminResource
+    public function get(string $resourceName): Admin
     {
         if (!$this->has($resourceName)) {
             throw new Exception('Resource with name "'.$resourceName.'" not found');
@@ -41,11 +61,13 @@ class ResourceRegistry implements ResourceRegistryInterface
 
     public function all(): array
     {
+        $this->load();
+
         return $this->resources;
     }
 
     public function getResourceNames(): array
     {
-        return array_keys($this->resources);
+        return \array_keys($this->resources);
     }
 }
