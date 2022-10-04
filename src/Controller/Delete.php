faae1570215b6918640f5b17d6a1d200dc410f5e
Arnaud Frézet <arnaudfrezet@gmail.com>
2022-10-04 18:35:55 +0200

    feature: add new controllers separated by operation instead of the legacy global controller

---
+++ src/Controller/Delete.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace LAG\AdminBundle\Controller;
+
+class Delete extends AbstractOperationController
+{
+}
---
+++ src/Controller/Show.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LAG\AdminBundle\Controller;
+
+class Show
+{
+
+}
