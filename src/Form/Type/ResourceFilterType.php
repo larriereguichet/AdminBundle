d5b66d98cd7851bc6013c8f27d2698d223851771
Arnaud Frézet <arnaudfrezet@gmail.com>
2022-10-04 18:55:55 +0200

    feature: add operation and resource form type

---
+++ src/Form/Type/ResourceFilterType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LAG\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class ResourceFilterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+            $data = $event->getData();
+        });
+    }
+}
