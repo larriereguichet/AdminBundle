683eef395063d66a6b0277ccb04fef6847d8d720
Arnaud Frézet <arnaudfrezet@gmail.com>
2022-10-07 20:05:41 +0200

    feature: add validation on actions before rendering

--- src/Action/Render/ActionRenderer.php
+++ src/Action/Render/ActionRenderer.php
@@ -2,14 +2,18 @@
 
 namespace LAG\AdminBundle\Action\Render;
 
+use LAG\AdminBundle\Exception\Validation\InvalidActionException;
 use LAG\AdminBundle\Metadata\Action;
 use LAG\AdminBundle\Routing\UrlGenerator\UrlGeneratorInterface;
+use Symfony\Component\Validator\Constraints\Valid;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Twig\Environment;
 
 class ActionRenderer implements ActionRendererInterface
 {
     public function __construct(
         private UrlGeneratorInterface $urlGenerator,
+        private ValidatorInterface $validator,
         private Environment $environment,
     ) {
     }
@@ -19,10 +23,24 @@ class ActionRenderer implements ActionRendererInterface
         mixed $data = null,
         array $options = []
     ): string {
+        $errors = $this->validator->validate($action, [new Valid()]);
+
+        if ($errors->count() > 0) {
+            throw new InvalidActionException($errors);
+        }
+
         if ($action->getRouteName()) {
-            $url = $this->urlGenerator->generateFromRouteName($action->getRouteName(), $action->getRouteParameters(), $data);
+            $url = $this->urlGenerator->generateFromRouteName(
+                $action->getRouteName(),
+                $action->getRouteParameters(),
+                $data,
+            );
         } else {
-            $url = $this->urlGenerator->generateFromOperationName($action->getResourceName(), $action->getOperationName(), $data);
+            $url = $this->urlGenerator->generateFromOperationName(
+                $action->getResourceName(),
+                $action->getOperationName(),
+                $data,
+            );
         }
 
         return $this->environment->render($action->getTemplate(), [
