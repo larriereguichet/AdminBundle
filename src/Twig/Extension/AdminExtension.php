7082af89fe83a57349e19ddf44b16f60978ec5d1
Arnaud Frézet <arnaudfrezet@gmail.com>
2022-10-12 19:41:40 +0200

    feature: add twig action to check operation grants and remove legacy twig functions

--- src/Twig/Extension/AdminExtension.php
+++ src/Twig/Extension/AdminExtension.php
@@ -16,8 +16,6 @@ use Twig\TwigFunction;
 class AdminExtension extends AbstractExtension
 {
     public function __construct(
-        private bool $mediaEnabled,
-        private ApplicationConfiguration $applicationConfiguration,
         private SecurityHelper $security,
         private ActionRendererInterface $actionRenderer,
         private UrlGeneratorInterface $urlGenerator,
@@ -27,42 +25,15 @@ class AdminExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('admin_config', [$this, 'getApplicationParameter']),
-            new TwigFunction('admin_action_allowed', [$this, 'isAdminActionAllowed']),
-            new TwigFunction('admin_media_enabled', [$this, 'isMediaBundleEnabled']),
-            new TwigFunction('admin_is_translation_enabled', [$this, 'isTranslationEnabled']),
+            new TwigFunction('lag_admin_operation_allowed', [$this, 'isOperationAllowed']),
             new TwigFunction('lag_admin_action', [$this, 'renderAction'], ['is_safe' => ['html']]),
             new TwigFunction('lag_admin_operation_url', [$this, 'getOperationUrl'], ['is_safe' => ['html']]),
         ];
     }
 
-    public function getApplicationParameter($name)
+    public function isOperationAllowed(string $resourceName, string $operationName): bool
     {
-        return $this
-            ->applicationConfiguration
-            ->get($name)
-        ;
-    }
-
-    /**
-     * Return true if the given action is allowed for the given Admin.
-     */
-    public function isAdminActionAllowed(string $adminName, string $actionName): bool
-    {
-        return $this->security->isActionAllowed($adminName, $actionName);
-    }
-
-    /**
-     * Return true if the media bundle is enabled.
-     */
-    public function isMediaBundleEnabled(): bool
-    {
-        return $this->mediaEnabled;
-    }
-
-    public function isTranslationEnabled(): bool
-    {
-        return $this->applicationConfiguration->isTranslationEnabled();
+        return $this->security->isOperationAllowed($resourceName, $operationName);
     }
 
     public function renderAction(Action $action, mixed $data = null, array $options = []): string
