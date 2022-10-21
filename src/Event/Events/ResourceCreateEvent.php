65327ca76b5148694553506c0f357a35789697f0
Arnaud Frézet <arnaudfrezet@gmail.com>
2022-10-21 23:49:48 +0200

    feature: add resource create and created events

--- src/Event/Events/ResourceCreateEvent.php
+++ src/Event/Events/ResourceCreateEvent.php
@@ -2,16 +2,13 @@
 
 declare(strict_types=1);
 
-namespace LAG\AdminBundle\Event;
+namespace LAG\AdminBundle\Event\Events;
 
 use LAG\AdminBundle\Metadata\AdminResource;
 use Symfony\Contracts\EventDispatcher\Event;
 
-class AdminEvent extends Event
+class ResourceCreateEvent extends Event
 {
-    public const ADMIN_CREATE = 'lag_admin.admin.create';
-    public const ADMIN_CREATED = 'lag_admin.admin.created';
-
     public function __construct(private AdminResource $resource)
     {
     }
