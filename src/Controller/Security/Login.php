2a2f82956c57ef0194cae2b06de7a7733a5144ba
johnkrovitch <arnaudfrezet@gmail.com>
2023-02-26 11:41:21 +0100

    chore: move login controller to the security namespace

--- src/Controller/Security/Login.php
+++ src/Controller/Security/Login.php
@@ -2,29 +2,34 @@
 
 declare(strict_types=1);
 
-namespace LAG\AdminBundle\Controller;
+namespace LAG\AdminBundle\Controller\Security;
 
 use LAG\AdminBundle\Form\Type\Security\LoginType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Twig\Environment;
 
-class LoginAction
+class Login
 {
     public function __construct(
         private FormFactoryInterface $formFactory,
+        private AuthenticationUtils $authenticationUtils,
         private Environment $environment,
     ) {
     }
 
-    public function __invoke(Request $request, string $template): Response
+    public function __invoke(Request $request): Response
     {
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+
         $form = $this->formFactory->create(LoginType::class);
         $form->handleRequest($request);
 
-        return new Response($this->environment->render($request->attributes->get('template'), [
+        return new Response($this->environment->render('@LAGAdmin/security/login.html.twig', [
             'form' => $form->createView(),
+            'error' => $error,
         ]));
     }
 }
