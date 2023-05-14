8fff5f1a965697b20a6d467a8a8d0fcfb21cd596
johnkrovitch <arnaudfrezet@gmail.com>
2023-05-14 23:02:48 +0200

    feature: add default form options for the filter form type

--- src/EventListener/Operation/OperationCreateListener.php
+++ src/EventListener/Operation/OperationCreateListener.php
@@ -6,10 +6,12 @@ namespace LAG\AdminBundle\EventListener\Operation;
 
 use LAG\AdminBundle\Event\Events\OperationEvent;
 use LAG\AdminBundle\Form\Type\OperationDataType;
-use LAG\AdminBundle\Form\Type\ResourceFilterType;
+use LAG\AdminBundle\Form\Type\Resource\DeleteType;
+use LAG\AdminBundle\Form\Type\Resource\FilterType;
 use LAG\AdminBundle\Metadata\AdminResource;
 use LAG\AdminBundle\Metadata\CollectionOperationInterface;
 use LAG\AdminBundle\Metadata\Create;
+use LAG\AdminBundle\Metadata\Delete;
 use LAG\AdminBundle\Metadata\Index;
 use LAG\AdminBundle\Metadata\Link;
 use LAG\AdminBundle\Metadata\OperationInterface;
@@ -31,48 +33,59 @@ class OperationCreateListener
             }
 
             if ($operation instanceof Index && \count($operation->getFilters() ?? []) > 0) {
-                $operation = $operation->withFormType(ResourceFilterType::class);
+                $operation = $operation->withFormType(FilterType::class);
             }
         }
 
-        if (is_a($operation->getFormType(), ResourceFilterType::class, true) && !\array_key_exists('operation', $operation->getFormOptions())) {
-            $operation = $operation->withFormOptions([
-                'operation' => $operation,
-            ]);
+        if (is_a($operation->getFormType(), FilterType::class, true) && !\array_key_exists('operation', $operation->getFormOptions())) {
+            $operation = $operation->withFormOptions(['operation' => $operation]);
+        }
+
+        if ($operation instanceof Delete && $operation->getFormType() === DeleteType::class) {
+            $operation = $operation->withFormOptions(array_merge($operation->getFormOptions(), ['resource' => $resource]));
         }
 
         if ($operation instanceof CollectionOperationInterface) {
             if ($operation->getFilters() === null) {
                 $operation = $operation->withFilters([]);
             }
-
-            if ($operation->getItemActions() === null) {
-                $operation = $this->withDefaultItemActions($resource, $operation);
-            }
         }
+
+        if (!$operation->getItemActions()) {
+            $operation = $this->withDefaultItemActions($resource, $operation);
+        }
+
         $event->setOperation($operation);
     }
 
-    private function withDefaultItemActions(AdminResource $resource, CollectionOperationInterface $operation): OperationInterface
+    private function withDefaultItemActions(AdminResource $resource, OperationInterface $operation): OperationInterface
     {
         $actions = [];
 
-        if ($resource->hasOperation('update')) {
-            $actions[] = new Link(
-                resourceName: $resource->getName(),
-                operationName: 'update',
-                label: 'lag_admin.resource.update',
-                type: 'secondary'
-            );
-        }
+        if ($operation instanceof CollectionOperationInterface) {
+            if ($resource->hasOperation('update')) {
+                $actions[] = new Link(
+                    resourceName: $resource->getName(),
+                    operationName: 'update',
+                    label: 'lag_admin.ui.update',
+                );
+            }
 
-        if ($resource->hasOperation('delete')) {
-            $actions[] = new Link(
-                resourceName: $resource->getName(),
-                operationName: 'delete',
-                label: 'lag_admin.resource.delete',
-                type: 'danger'
-            );
+            if ($resource->hasOperation('delete')) {
+                $actions[] = new Link(
+                    resourceName: $resource->getName(),
+                    operationName: 'delete',
+                    label: 'lag_admin.ui.delete',
+                );
+            }
+        } else {
+            if ($resource->hasOperation('index')) {
+                $actions[] = new Link(
+                    resourceName: $resource->getName(),
+                    operationName: 'index',
+                    label: 'lag_admin.ui.cancel',
+                );
+            }
         }
 
         return $operation->withItemActions($actions);
