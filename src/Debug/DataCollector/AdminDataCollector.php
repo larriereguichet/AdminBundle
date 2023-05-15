a757621313dbd246e71afef59d95046c92dce93e
johnkrovitch <arnaudfrezet@gmail.com>
2023-05-15 18:42:34 +0200

    feature: update the debug data collector to match the new metadata system

--- src/Debug/DataCollector/AdminDataCollector.php
+++ src/Debug/DataCollector/AdminDataCollector.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace LAG\AdminBundle\Debug\DataCollector;
 
 use LAG\AdminBundle\Application\Configuration\ApplicationConfiguration;
+use LAG\AdminBundle\Metadata\AdminResource;
+use LAG\AdminBundle\Metadata\OperationInterface;
 use LAG\AdminBundle\Request\Extractor\ParametersExtractorInterface;
 use LAG\AdminBundle\Resource\Registry\ResourceRegistryInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,13 +30,7 @@ class AdminDataCollector extends DataCollector
         ];
 
         foreach ($this->registry->all() as $resource) {
-            $reflection = new \ReflectionClass($resource);
-
-            foreach ($reflection->getProperties() as $reflectionProperty) {
-                if ($reflectionProperty->isInitialized($resource)) {
-                    $data['resources'][$resource->getName()][$reflectionProperty->getName()] = $reflectionProperty->getValue($resource);
-                }
-            }
+            $data['resources'][$resource->getName()] = $this->collectResource($resource);
         }
 
         // When the application configuration is not defined or resolved, we can not access to the admin/menus
@@ -64,5 +60,56 @@ class AdminDataCollector extends DataCollector
     public function getData(): array
     {
         return $this->data;
+    }
+
+    private function collectResource(AdminResource $resource): array
+    {
+        $operations = [];
+
+        foreach ($resource->getOperations() as $operation) {
+            $operations[$operation->getName()] = $this->collectOperation($operation);
+        }
+
+        return [
+            'name' => $resource->getName(),
+            'dataClass' => $resource->getDataClass(),
+            'title' => $resource->getTitle(),
+            'group' => $resource->getGroup(),
+            'icon' => $resource->getIcon(),
+            'operations' => $operations,
+            'processor' => $resource->getProcessor(),
+            'provider' => $resource->getProvider(),
+            'identifiers' => $resource->getIdentifiers(),
+            'routePattern' => $resource->getRoutePattern(),
+            'routePrefix' => $resource->getRoutePrefix(),
+            'translationPattern' => $resource->getTranslationPattern(),
+            'translationDomain' => $resource->getTranslationDomain(),
+        ];
+    }
+
+    private function collectOperation(OperationInterface $operation): array
+    {
+        return [
+            'name' => $operation->getName(),
+            'title' => $operation->getTitle(),
+            'description' => $operation->getDescription(),
+            'icon' => $operation->getIcon(),
+            'template' => $operation->getTemplate(),
+            'permissions' => $operation->getPermissions(),
+            'controller' => $operation->getController(),
+            'route' => $operation->getRoute(),
+            'routeParameters' => $operation->getRouteParameters(),
+            'methods' => $operation->getMethods(),
+            'path' => $operation->getPath(),
+            'targetRoute' => $operation->getTargetRoute(),
+            'targetRouteParameters' => $operation->getRouteParameters(),
+            // 'properties' => $operation->getProperties(),
+            'formType' => $operation->getFormType(),
+            'processor' => $operation->getProcessor(),
+            'provider' => $operation->getProvider(),
+            'identifiers' => $operation->getIdentifiers(),
+            // 'contextualActions' => $operation->getContextualActions(),
+            // 'itemActions' => $operation->getItemActions(),
+        ];
     }
 }
