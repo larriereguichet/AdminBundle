d4de70f361f81785a70c13ed0187460630a7a941
johnkrovitch <arnaudfrezet@gmail.com>
2023-05-15 18:39:30 +0200

    chore: move form resource types to their own namespace

---
+++ src/Form/Type/Resource/DeleteType.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LAG\AdminBundle\Form\Type\Resource;
+
+use LAG\AdminBundle\Metadata\AdminResource;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DeleteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        foreach ($options['resource']->getIdentifiers() as $identifier) {
+            $builder->add($identifier, HiddenType::class);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver
+            ->setDefaults([
+                'label' => false,
+            ])
+            ->setRequired('resource')
+            ->setAllowedTypes('resource', AdminResource::class)
+        ;
+    }
+}
