07df2bf037a74f80b43e5f946e8dcca9673a0f18
johnkrovitch <arnaudfrezet@gmail.com>
2023-10-26 23:30:15 +0200

    feature: gather each operations controller into a generic controller for resources items and collections as the previous are replaced by the new resource system

--- src/Controller/Resource/ResourceCollectionController.php
+++ src/Controller/Resource/ResourceCollectionController.php
@@ -1,68 +1,57 @@
 <?php
 
-declare(strict_types=1);
+namespace LAG\AdminBundle\Controller\Resource;
 
-namespace LAG\AdminBundle\Controller;
-
-use LAG\AdminBundle\Exception\Operation\InvalidCollectionOperationException;
 use LAG\AdminBundle\Grid\Factory\GridFactoryInterface;
-use LAG\AdminBundle\Metadata\AdminResource;
 use LAG\AdminBundle\Metadata\CollectionOperationInterface;
 use LAG\AdminBundle\Request\Context\ContextProviderInterface;
 use LAG\AdminBundle\Request\Uri\UriVariablesExtractorInterface;
-use LAG\AdminBundle\State\DataProviderInterface;
+use LAG\AdminBundle\State\Provider\DataProviderInterface;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 use Twig\Environment;
 
-class Index
+readonly class ResourceCollectionController
 {
     public function __construct(
         private UriVariablesExtractorInterface $uriVariablesExtractor,
         private ContextProviderInterface $contextProvider,
         private DataProviderInterface $dataProvider,
-        private FormFactoryInterface $formFactory,
         private GridFactoryInterface $gridFactory,
+        private FormFactoryInterface $formFactory,
+        private SerializerInterface $serializer,
         private Environment $environment,
     ) {
     }
 
-    public function __invoke(Request $request, AdminResource $resource): Response
+    public function __invoke(Request $request, CollectionOperationInterface $operation): Response
     {
-        $operation = $this->getOperation($resource);
         $uriVariables = $this->uriVariablesExtractor->extractVariables($operation, $request);
         $context = $this->contextProvider->getContext($operation, $request);
         $form = null;
 
-        if ($operation->getFormType()) {
-            $form = $this->formFactory->create($operation->getFormType(), [], $operation->getFormOptions());
+        if ($operation->getFilterFormType() !== null) {
+            $form = $this->formFactory->create($operation->getFilterFormType(), [], $operation->getFilterFormOptions());
             $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                $context['filters'] = $form->getData();
-            }
         }
         $data = $this->dataProvider->provide($operation, $uriVariables, $context);
         $grid = $this->gridFactory->create($operation, $data);
 
+        if ($context['json']) {
+            $content = $this->serializer->serialize($data, 'json', $operation->getNormalizationContext());
+
+            return new JsonResponse($content, Response::HTTP_OK, [], true);
+        }
+
         return new Response($this->environment->render($operation->getTemplate(), [
             'grid' => $grid,
-            'resource' => $resource,
+            'resource' => $operation->getResource(),
             'operation' => $operation,
             'data' => $data,
             'form' => $form?->createView(),
         ]));
-    }
-
-    private function getOperation(AdminResource $resource): CollectionOperationInterface
-    {
-        $operation = $resource->getCurrentOperation();
-
-        if (!$operation instanceof CollectionOperationInterface) {
-            throw new InvalidCollectionOperationException($operation);
-        }
-
-        return $operation;
     }
 }
