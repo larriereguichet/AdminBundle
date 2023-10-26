12d92b16a73e8c0225a80593bf0e037ffc452f04
johnkrovitch <arnaudfrezet@gmail.com>
2023-10-26 23:24:09 +0200

    feature: add request resolver to load resource operations into controllers

---
+++ src/Request/Resolver/OperationValueResolver.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LAG\AdminBundle\Request\Resolver;
+
+use LAG\AdminBundle\Metadata\Context\ResourceContextInterface;
+use LAG\AdminBundle\Metadata\OperationInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+class OperationValueResolver implements ValueResolverInterface
+{
+    public function __construct(
+        private ResourceContextInterface $resourceContext,
+    )
+    {
+    }
+
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
+    {
+        if (!$this->resourceContext->supports($request) || !is_a($argument->getType(), OperationInterface::class)) {
+            return [];
+        }
+
+        yield $this->resourceContext->getOperation($request);
+    }
+}
