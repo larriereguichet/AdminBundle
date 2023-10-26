c200c918f3159f7d2c995b063c0f7dc2c275dccb
johnkrovitch <arnaudfrezet@gmail.com>
2023-10-26 23:30:15 +0200

    feature: gather each operations controller into a generic controller for resources items and collections as the previous are replaced by the new resource system

--- src/Controller/Resource/ResourceController.php
+++ src/Controller/Resource/ResourceController.php
@@ -2,21 +2,22 @@
 
 declare(strict_types=1);
 
-namespace LAG\AdminBundle\Controller;
+namespace LAG\AdminBundle\Controller\Resource;
 
-use LAG\AdminBundle\Metadata\AdminResource;
+use LAG\AdminBundle\Metadata\OperationInterface;
 use LAG\AdminBundle\Request\Context\ContextProviderInterface;
 use LAG\AdminBundle\Request\Uri\UriVariablesExtractorInterface;
-use LAG\AdminBundle\Routing\UrlGenerator\UrlGeneratorInterface;
-use LAG\AdminBundle\State\DataProcessorInterface;
-use LAG\AdminBundle\State\DataProviderInterface;
+use LAG\AdminBundle\Response\Handler\RedirectHandlerInterface;
+use LAG\AdminBundle\State\Processor\DataProcessorInterface;
+use LAG\AdminBundle\State\Provider\DataProviderInterface;
 use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 use Twig\Environment;
 
-class ItemOperationController
+readonly class ResourceController
 {
     public function __construct(
         private UriVariablesExtractorInterface $uriVariablesExtractor,
@@ -25,38 +26,45 @@ class ItemOperationController
         private DataProcessorInterface $dataProcessor,
         private FormFactoryInterface $formFactory,
         private Environment $environment,
-        private UrlGeneratorInterface $urlGenerator,
+        private RedirectHandlerInterface $redirectionHandler,
+        private SerializerInterface $serializer,
     ) {
     }
 
-    public function __invoke(Request $request, AdminResource $resource): Response
+    public function __invoke(Request $request, OperationInterface $operation): Response
     {
-        $operation = $resource->getCurrentOperation();
         $uriVariables = $this->uriVariablesExtractor->extractVariables($operation, $request);
         $context = $this->contextProvider->getContext($operation, $request);
         $data = $this->dataProvider->provide($operation, $uriVariables, $context);
         $form = null;
 
-        if ($operation->getFormType() != null) {
+        if ($operation->getFormType() !== null) {
             $form = $this->formFactory->create($operation->getFormType(), $data, $operation->getFormOptions());
             $form->handleRequest($request);
+
+            if ($context['json']) {
+                $form->submit($request->toArray());
+            }
 
             if ($form->isSubmitted() && $form->isValid()) {
                 $data = $form->getData();
                 $this->dataProcessor->process($data, $operation, $uriVariables, $context);
 
-                return new RedirectResponse($this->urlGenerator->generateFromRouteName(
-                    $operation->getTargetRoute() ?? $operation->getRoute(),
-                    $operation->getTargetRouteParameters() ?? $operation->getRouteParameters(),
-                ));
+                return $this->redirectionHandler->createRedirectResponse($operation, $data);
             }
         }
 
+        if ($request->getContentTypeFormat() === 'application/json') {
+            $content = $this->serializer->serialize($data, 'json', $operation->getNormalizationContext());
+
+            return new JsonResponse($content, Response::HTTP_OK, [], true);
+        }
+
         return new Response($this->environment->render($operation->getTemplate(), [
-            'resource' => $resource,
+            'resource' => $operation->getResource(),
             'operation' => $operation,
             'data' => $data,
             'form' => $form?->createView(),
-        ]));
+        ]), $form->isSubmitted() ? Response::HTTP_UNPROCESSABLE_ENTITY : Response::HTTP_OK);
     }
 }
