106e0c2c7142af820eb65aa587db16585fdd1f35
johnkrovitch <arnaudfrezet@gmail.com>
2023-10-28 22:19:51 +0200

    chore: simplify make commands names

--- make/php.mk
+++ make/php.mk
@@ -1,9 +1,9 @@
 .PHONY: tests
 
-tests: phpunit.run phpstan.run var-dump-checker.run php-cs-fixer.run
+tests: phpunit phpstan var-dump-checker cs
 
 # PHPUnit
-phpunit.run:
+phpunit:
 	$(PHP) bin/phpunit
 	@echo "Results file generated file://$(current_dir)/var/phpunit/coverage/index.html"
 
@@ -12,22 +12,20 @@ phpunit.stop-on-failure:
 	@echo "Results file generated file://$(current_dir)/var/phpunit/coverage/index.html"
 
 # PHPCSFixer
-cs.fix: php-cs-fixer.fix
-
-php-cs-fixer.fix:
+cs.fix:
 	PHP_CS_FIXER_IGNORE_ENV=1 $(PHP) bin/php-cs-fixer fix --using-cache=no --diff --allow-risky=yes --config .php-cs-fixer.dist.php
 
-php-cs-fixer.run:
+cs:
 	PHP_CS_FIXER_IGNORE_ENV=1 $(PHP) bin/php-cs-fixer fix --using-cache=no --diff --allow-risky=yes --config .php-cs-fixer.dist.php --dry-run
 
 # phpstan
-phpstan.run:
+phpstan:
 	$(PHP) bin/phpstan analyse --level=5 src
 
 # Misc
 bc.check:
 	$(PHP) bin/roave-backward-compatibility-check
 
-var-dump-checker.run:
+var-dump-checker:
 	$(PHP) bin/var-dump-check --symfony src
 	$(PHP) bin/var-dump-check --symfony tests
