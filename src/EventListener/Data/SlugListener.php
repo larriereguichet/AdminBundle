f9ef1f1ef1c46745d97639818fa0c8708ac43f07
johnkrovitch <arnaudfrezet@gmail.com>
2023-10-28 11:54:20 +0200

    feature: change the slug listener to have a more flexible slug configuration

--- src/EventListener/Data/SlugListener.php
+++ src/EventListener/Data/SlugListener.php
@@ -4,18 +4,32 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\EventListener\Data;
 
-use LAG\AdminBundle\Entity\SluggableInterface;
 use LAG\AdminBundle\Event\Events\DataEvent;
+use LAG\AdminBundle\Slug\Generator\SlugGeneratorInterface;
+use LAG\AdminBundle\Slug\Mapping\SlugMappingInterface;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class SlugListener
 {
+    public function __construct(
+        private SlugGeneratorInterface $generator,
+        private SlugMappingInterface $mapping,
+    ) {
+    }
+
     public function __invoke(DataEvent $event): void
     {
         $data = $event->getData();
 
-        if (!$data instanceof SluggableInterface || !$data->getSlugSource()) {
+        if (!$this->mapping->hasMapping(get_class($data))) {
             return;
         }
-        $data->generateSlug($data->getSlugSource());
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        foreach ($this->mapping->getMapping(get_class($data)) as $mapping) {
+            $source = $accessor->getValue($data, $mapping->sourceProperty);
+            $slug = $this->generator->generateSlug($source, $mapping->generator);
+            $accessor->setValue($data, $mapping->targetProperty, $slug);
+        }
     }
 }
