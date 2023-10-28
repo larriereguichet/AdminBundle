8b71affe816216e76dad64e54f16b47ef046fef4
johnkrovitch <arnaudfrezet@gmail.com>
2023-10-28 12:01:19 +0200

    feature: add basic stimulus controllers to handle modals, tabs, and select interactions

--- assets/js/controllers/lag_admin_select2_controller.js
+++ assets/js/controllers/lag_admin_select2_controller.js
@@ -8,6 +8,12 @@ export default class extends Controller {
         new TomSelect(this.element, {
             create: this.element.dataset.allowAdd ?? true,
             maxItems: this.element.dataset.multiple ?? null ? null : 1,
+            render: {
+                item: function(data, escape) {
+                    console.log(data)
+                    return '<span class="item badge me-1 bg-secondary" title="' + escape(data.text) + '">' + escape(data.text) + '</span>';
+                }
+            }
         })
     }
 }
