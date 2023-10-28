498fd3b772ae867199b3e8b6251fbea309a31d6d
johnkrovitch <arnaudfrezet@gmail.com>
2023-10-28 11:52:46 +0200

    chore: rename the operation permissions listener into a more appropriate name

---
+++ src/EventListener/Security/OperationPermissionListener.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LAG\AdminBundle\EventListener\Security;
+
+use LAG\AdminBundle\Metadata\AdminResource;
+use LAG\AdminBundle\Metadata\Context\ResourceContextInterface;
+use LAG\AdminBundle\Security\Voter\OperationPermissionVoter;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpKernel\Event\ControllerArgumentsEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class OperationPermissionListener
+{
+    public function __construct(
+        private ResourceContextInterface $resourceContext,
+        private Security $security,
+    ) {
+    }
+
+    public function __invoke(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        if (!$this->resourceContext->supports($request)) {
+            return;
+        }
+        $operation = $this->resourceContext->getOperation($request);
+
+        if (!$this->security->isGranted(OperationPermissionVoter::RESOURCE_ACCESS, $operation)) {
+            throw new AccessDeniedException('You are not allowed to access to this admin resource');
+        }
+    }
+}
