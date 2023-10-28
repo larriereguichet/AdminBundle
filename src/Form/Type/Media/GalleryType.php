53ffc64a286fe77a074884cdcfe7033bdbc697eb
johnkrovitch <arnaudfrezet@gmail.com>
2023-10-28 11:37:21 +0200

    chore: re-import forms from the media bundle as it will be removed

--- src/Form/Type/Media/GalleryType.php
+++ src/Form/Type/Media/GalleryType.php
@@ -4,27 +4,32 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\Form\Type\Media;
 
+use LAG\AdminBundle\Entity\Image;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class GalleryType extends AbstractType
 {
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver
             ->setDefaults([
                 'attr' => [
                     'class' => 'media-gallery-input',
                 ],
-                'multiple' => false,
+                'class' => Image::class,
             ])
-            ->setAllowedTypes('multiple', 'boolean')
         ;
     }
 
     public function getParent(): string
     {
         return EntityType::class;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'lag_admin_gallery';
     }
 }
