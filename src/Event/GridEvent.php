c7afdeaa276cf8ab7496658e0d0a65c59366d8a2
johnkrovitch <arnaudfrezet@gmail.com>
2023-10-28 11:53:35 +0200

    chore: small namespace and methods naming changes after refactoring

--- src/Event/GridEvent.php
+++ src/Event/GridEvent.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\Event;
 
-use LAG\AdminBundle\Grid\Grid;
+use LAG\AdminBundle\Grid\GridView;
 use Symfony\Contracts\EventDispatcher\Event;
 
 class GridEvent extends Event
@@ -12,11 +12,11 @@ class GridEvent extends Event
     public const GRID_CREATED = 'lag_admin.grid.created';
 
     public function __construct(
-        private Grid $grid
+        private GridView $grid
     ) {
     }
 
-    public function getGrid(): Grid
+    public function getGrid(): GridView
     {
         return $this->grid;
     }
