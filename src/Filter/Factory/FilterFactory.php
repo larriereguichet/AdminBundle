f7797bd4152570650cbba7ee0eba1b460eb8e651
johnkrovitch <arnaudfrezet@gmail.com>
2023-10-28 11:49:52 +0200

    feature: add a filter factory to allow resource collection to be filtered

--- src/Filter/Factory/FilterFactory.php
+++ src/Filter/Factory/FilterFactory.php
@@ -9,7 +9,7 @@ use LAG\AdminBundle\Metadata\Filter\Filter;
 use LAG\AdminBundle\Metadata\Filter\FilterInterface;
 use LAG\AdminBundle\Metadata\Filter\StringFilter;
 use LAG\AdminBundle\Metadata\Property\PropertyInterface;
-use LAG\AdminBundle\Metadata\Property\StringProperty;
+use LAG\AdminBundle\Metadata\Property\Text;
 use Symfony\Component\Validator\Constraints\Valid;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -20,8 +20,9 @@ class FilterFactory implements FilterFactoryInterface
     ) {
     }
 
-    public function create(FilterInterface $filter): FilterInterface
+    public function create(FilterInterface $filterDefinition): FilterInterface
     {
+        $filter = $this->initializeFilter($filterDefinition);
         $errors = $this->validator->validate($filter, [new Valid()]);
 
         if ($errors->count() > 0) {
@@ -36,13 +37,24 @@ class FilterFactory implements FilterFactoryInterface
         $class = Filter::class;
         $name = $property->getName();
 
-        if ($property instanceof StringProperty) {
+        if ($property instanceof Text) {
             $class = StringFilter::class;
         }
 
-        return new $class(
+        $definition = new $class(
             name: $name,
             propertyPath: $property->getPropertyPath(),
         );
+
+        return $this->create($definition);
+    }
+
+    private function initializeFilter(FilterInterface $filter): FilterInterface
+    {
+        if ($filter->getPropertyPath() === null) {
+            $filter = $filter->withPropertyPath($filter->getName());
+        }
+
+        return $filter;
     }
 }
