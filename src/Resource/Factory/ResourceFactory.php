7af9602cb2fe989e07e16c92fcb00d31baf5a512
johnkrovitch <arnaudfrezet@gmail.com>
2024-07-22 13:57:47 +0200

    feature: add a resource factory to handle resource creation and ensuring created resources are valid

---
+++ src/Resource/Factory/ResourceFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LAG\AdminBundle\Resource\Factory;
+
+use LAG\AdminBundle\Exception\Exception;
+use LAG\AdminBundle\Exception\InvalidResourceException;
+use LAG\AdminBundle\Resource\Metadata\Resource;
+use Symfony\Component\Validator\Constraints\Valid;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+final readonly class ResourceFactory implements ResourceFactoryInterface
+{
+    public function __construct(
+        private OperationFactoryInterface $operationFactory,
+        private ValidatorInterface $validator,
+    ) {
+    }
+
+    public function create(Resource $definition): Resource
+    {
+        $operations = [];
+
+        foreach ($definition->getOperations() as $operation) {
+            // Ensure the operation belongs to the right resource
+            $operations[] = $this->operationFactory->create($operation->withResource($definition));
+        }
+        $resource = $definition->withOperations($operations);
+
+        $errors = $this->validator->validate($resource, [new Valid()]);
+
+        if ($errors->count() > 0) {
+            throw new InvalidResourceException($resource->getName(), $errors);
+        }
+
+        return $resource;
+    }
+}
