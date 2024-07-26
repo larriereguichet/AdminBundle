afc3afc7caa06d146174693dad6c849ffbc17c0d
johnkrovitch <arnaudfrezet@gmail.com>
2024-07-26 12:21:56 +0200

    feature: add a resource event dispatcher to ease multiple specific and generic events dispatching

--- src/Event/ResourceEvents.php
+++ src/Event/ResourceEvents.php
@@ -6,9 +6,6 @@ namespace LAG\AdminBundle\Event;
 
 class ResourceEvents
 {
-    public const RESOURCE_CREATE = 'lag_admin.resource.create';
-    public const RESOURCE_CREATED = 'lag_admin.resource.created';
-    public const RESOURCE_CREATE_PATTERN = 'lag_admin.resource.%s.create';
-    public const RESOURCE_CREATED_PATTERN = 'lag_admin.resource.%s.created';
-    public const RESOURCE_COLLECTION_LOADED = 'lag_admin.resources.loaded';
+    public const string RESOURCE_CREATE = 'resource.create';
+    public const string RESOURCE_CREATED = 'resource.created';
 }
