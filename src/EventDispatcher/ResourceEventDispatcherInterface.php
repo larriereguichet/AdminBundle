808393a0b43786bbc2104e1f695e8068e1c61c61
johnkrovitch <arnaudfrezet@gmail.com>
2024-08-08 20:52:05 +0200

    chore: set up the resource event dispatcher as final

--- src/EventDispatcher/ResourceEventDispatcherInterface.php
+++ src/EventDispatcher/ResourceEventDispatcherInterface.php
@@ -6,15 +6,6 @@ namespace LAG\AdminBundle\EventDispatcher;
 
 use Symfony\Contracts\EventDispatcher\Event;
 
-/**
- * Add a single entrypoint to dispatch resources and operations generics and specifics events.
- *
- * For instance, if the "my_resource" resource of "my_application" application is created, the following events should
- * be dispatched:
- * - lag_admin.resource.created (generic)
- * - my_application.my_resource.created (specific)
- * - lag_admin.my_resource.created (specific)
- */
 interface ResourceEventDispatcherInterface
 {
     public function dispatchResourceEvents(
@@ -22,6 +13,14 @@ interface ResourceEventDispatcherInterface
         string $eventName,
         string $applicationName,
         string $resourceName,
-        ?string $operationName = null,
+        string $operationName,
+    ): void;
+
+    public function dispatchGridEvents(
+        Event $event,
+        string $eventName,
+        string $applicationName,
+        string $resourceName,
+        string $gridName,
     ): void;
 }
