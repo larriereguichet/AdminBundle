268bebf8837ceb5f39f3c0bf6919f864b818ac3f
johnkrovitch <arnaudfrezet@gmail.com>
2024-08-09 14:01:02 +0200

    tests: improve metadata tests

--- tests/phpunit/Metadata/Factory/OperationFactoryTest.php
+++ tests/phpunit/Metadata/Factory/OperationFactoryTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\Tests\Metadata\Factory;
 
-use LAG\AdminBundle\Event\OperationEvent;
-use LAG\AdminBundle\Event\OperationEvents;
 use LAG\AdminBundle\Filter\Factory\FilterFactoryInterface;
 use LAG\AdminBundle\Resource\Factory\OperationFactory;
 use LAG\AdminBundle\Resource\Metadata\Filter;
@@ -13,28 +11,28 @@ use LAG\AdminBundle\Resource\Metadata\Index;
 use LAG\AdminBundle\Resource\Metadata\Resource;
 use LAG\AdminBundle\Resource\Metadata\Text;
 use LAG\AdminBundle\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\MockObject\MockObject;
 
-class OperationFactoryTest extends TestCase
+final class OperationFactoryTest extends TestCase
 {
     private OperationFactory $factory;
     private MockObject $filterFactory;
 
-    public function testCreate(): void
+    #[Test]
+    public function itCreatesAnOperation(): void
     {
-        $definition = new Index(
-            name: 'index',
-            filters: [new Filter('my_filter')],
-        );
         $resource = new Resource(
             name: 'my_resource',
-            operations: [$definition],
             properties: [new Text('my_property')],
         );
+        $definition = (new Index(
+            name: 'index',
+            filters: [new Filter('my_filter')],
+        ))->withResource($resource);
 
-        $this
-            ->filterFactory
-            ->expects($this->once())
+        $this->filterFactory
+            ->expects(self::once())
             ->method('create')
         ;
 
@@ -43,7 +41,7 @@ class OperationFactoryTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->filterFactory = $this->createMock(FilterFactoryInterface::class);
+        $this->filterFactory = self::createMock(FilterFactoryInterface::class);
         $this->factory = new OperationFactory(
             $this->filterFactory,
         );
