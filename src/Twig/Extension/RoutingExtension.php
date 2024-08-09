265ce50b35c8681999bc3935dd57d0c56f3239bd
johnkrovitch <arnaudfrezet@gmail.com>
2024-08-09 14:09:02 +0200

    feature: split and update Twig extensions to use the new metadata system

---
+++ src/Twig/Extension/RoutingExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LAG\AdminBundle\Twig\Extension;
+
+use LAG\AdminBundle\View\Helper\RoutingHelper;
+use LAG\AdminBundle\View\Helper\RoutingHelperInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class RoutingExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly RoutingHelperInterface $helper,
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('lag_admin_path', [$this->helper, 'generatePath']),
+            new TwigFunction('lag_admin_url', [$this->helper, 'generateUrl']),
+            new TwigFunction('lag_admin_resource_url', [$this->helper, 'generateResourceUrl']),
+        ];
+    }
+}
--- tests/phpunit/Twig/Extension/AdminExtensionTest.php
+++ tests/phpunit/Twig/Extension/AdminExtensionTest.php
@@ -4,67 +4,46 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\Tests\Twig\Extension;
 
-use LAG\AdminBundle\Application\Configuration\ApplicationConfiguration;
-use LAG\AdminBundle\Grid\View\LinkRendererInterface;
-use LAG\AdminBundle\Metadata\AdminResource;
-use LAG\AdminBundle\Metadata\Get;
-use LAG\AdminBundle\Metadata\Link;
-use LAG\AdminBundle\Metadata\Registry\ResourceRegistryInterface;
+use LAG\AdminBundle\Resource\Metadata\Get;
+use LAG\AdminBundle\Resource\Metadata\Link;
+use LAG\AdminBundle\Resource\Metadata\Resource;
+use LAG\AdminBundle\Resource\Registry\ResourceRegistryInterface;
 use LAG\AdminBundle\Routing\UrlGenerator\UrlGeneratorInterface;
 use LAG\AdminBundle\Security\Voter\OperationPermissionVoter;
 use LAG\AdminBundle\Tests\TestCase;
 use LAG\AdminBundle\Twig\Extension\AdminExtension;
+use LAG\AdminBundle\View\Render\LinkRendererInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Bundle\SecurityBundle\Security;
 
 class AdminExtensionTest extends TestCase
 {
     private AdminExtension $adminExtension;
-    private MockObject $configuration;
     private MockObject $security;
     private MockObject $linkRenderer;
     private MockObject $urlGenerator;
     private MockObject $registry;
-
-    public function testServiceExists(): void
-    {
-        $this->assertServiceExists(AdminExtension::class);
-    }
-
     public function testGetFunctions(): void
     {
         foreach ($this->adminExtension->getFunctions() as $function) {
             $this->assertContains($function->getName(), [
-                'lag_admin_config',
                 'lag_admin_operation_allowed',
-                'lag_admin_action',
+                'lag_admin_link',
                 'lag_admin_operation_url',
+                'lag_admin_link_url',
             ]);
             $this->assertTrue(method_exists($this->adminExtension, $function->getCallable()[1]));
         }
     }
 
-    public function testGetConfigurationValue(): void
-    {
-        $this
-            ->configuration
-            ->expects($this->once())
-            ->method('get')
-            ->with('my_parameter')
-            ->willReturn('my_value')
-        ;
-
-        $this->assertEquals('my_value', $this->adminExtension->getConfigurationValue('my_parameter'));
-    }
-
     public function testIsOperationAllowed(): void
     {
         $operation = new Get(name: 'my_operation');
-        $resource = new AdminResource(name: 'my_resource', operations: [$operation]);
+        $resource = new Resource(name: 'my_resource', operations: [$operation]);
 
         $this
             ->registry
-            ->expects($this->once())
+            ->expects(self::once())
             ->method('get')
             ->with('my_resource')
             ->willReturn($resource)
@@ -72,7 +51,7 @@ class AdminExtensionTest extends TestCase
 
         $this
             ->security
-            ->expects($this->once())
+            ->expects(self::once())
             ->method('isGranted')
             ->with(OperationPermissionVoter::RESOURCE_ACCESS, $operation)
         ;
@@ -88,13 +67,13 @@ class AdminExtensionTest extends TestCase
 
         $this
             ->linkRenderer
-            ->expects($this->once())
+            ->expects(self::once())
             ->method('render')
             ->with($action)
             ->willReturn('<p>content</p>')
         ;
 
-        $content = $this->adminExtension->renderAction($action, $data, $options);
+        $content = $this->adminExtension->renderLink($action, $data, $options);
 
         $this->assertEquals('<p>content</p>', $content);
     }
@@ -105,26 +84,24 @@ class AdminExtensionTest extends TestCase
 
         $this
             ->urlGenerator
-            ->expects($this->once())
+            ->expects(self::once())
             ->method('generateFromOperationName')
             ->with('my_resource', 'my_operation', $data)
             ->willReturn('/url')
         ;
 
-        $url = $this->adminExtension->getOperationUrl('my_resource', 'my_operation', $data);
+        $url = $this->adminExtension->generateUrl('my_resource', 'my_operation', $data);
 
         $this->assertEquals('/url', $url);
     }
 
     protected function setUp(): void
     {
-        $this->configuration = $this->createMock(ApplicationConfiguration::class);
-        $this->security = $this->createMock(Security::class);
-        $this->linkRenderer = $this->createMock(LinkRendererInterface::class);
-        $this->urlGenerator = $this->createMock(UrlGeneratorInterface::class);
-        $this->registry = $this->createMock(ResourceRegistryInterface::class);
+        $this->security = self::createMock(Security::class);
+        $this->linkRenderer = self::createMock(LinkRendererInterface::class);
+        $this->urlGenerator = self::createMock(UrlGeneratorInterface::class);
+        $this->registry = self::createMock(ResourceRegistryInterface::class);
         $this->adminExtension = new AdminExtension(
-            $this->configuration,
             $this->security,
             $this->linkRenderer,
             $this->urlGenerator,
