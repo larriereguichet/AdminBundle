4b93191565f180a7917f2627e20a68b7fb9dc58e
johnkrovitch <arnaudfrezet@gmail.com>
2024-08-09 13:56:33 +0200

    tests: improve grids tests

--- tests/phpunit/Grid/View/LinkRendererTest.php
+++ tests/phpunit/Grid/View/LinkRendererTest.php
@@ -4,138 +4,68 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\Tests\Grid\View;
 
-use LAG\AdminBundle\Exception\Validation\InvalidActionException;
-use LAG\AdminBundle\Grid\View\LinkRenderer;
-use LAG\AdminBundle\Grid\View\LinkRendererInterface;
-use LAG\AdminBundle\Metadata\Link;
+use LAG\AdminBundle\Exception\InvalidLinkException;
+use LAG\AdminBundle\Resource\Metadata\Link;
 use LAG\AdminBundle\Routing\UrlGenerator\UrlGeneratorInterface;
 use LAG\AdminBundle\Tests\TestCase;
+use LAG\AdminBundle\View\Render\LinkRenderer;
+use LAG\AdminBundle\View\Render\LinkRendererInterface;
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Component\Validator\Constraints\Valid;
+use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Twig\Environment;
 
-class LinkRendererTest extends TestCase
+final class LinkRendererTest extends TestCase
 {
-    private LinkRendererInterface $actionRenderer;
+    private LinkRendererInterface $linkRenderer;
     private MockObject $urlGenerator;
     private MockObject $validator;
     private MockObject $environment;
 
-    public function testService(): void
+    #[Test]
+    public function itRendersLink(): void
     {
-        $this->assertServiceExists(LinkRenderer::class);
-        $this->assertServiceExists(LinkRendererInterface::class);
-    }
-
-    public function testRenderWithRouteName(): void
-    {
-        $action = new Link(
-            route: 'my_route',
-            routeParameters: ['id' => []],
-            template: 'my_template.html.twig',
-            resourceName: 'my_resource', // should not be used if route is provided
+        $link = new Link(
+            template: 'some_template.html.twig',
         );
-        $data = new \stdClass();
 
-        $this
-            ->validator
-            ->expects($this->once())
+        $this->validator
+            ->expects(self::once())
             ->method('validate')
-            ->with($action, [new Valid()])
-            ->willReturn($this->createMock(ConstraintViolationListInterface::class))
+            ->with($link, [new Valid()])
+            ->willReturn(new ConstraintViolationList())
         ;
-
-        $this
-            ->urlGenerator
-            ->expects($this->once())
-            ->method('generateFromRouteName')
-            ->with('my_route', ['id' => []], $data)
-            ->willReturn('/my-url')
+        $this->urlGenerator
+            ->expects(self::once())
+            ->method('generateUrl')
+            ->with($link)
+            ->willReturn('/some/url')
         ;
-
-        $this
-            ->environment
-            ->expects($this->once())
+        $this->environment
+            ->expects(self::once())
             ->method('render')
-            ->with('my_template.html.twig')
-            ->willReturn('<render></render>')
+            ->with('some_template.html.twig', [
+                'link' => $link->withUrl('/some/url'),
+                'options' => [],
+            ])
+            ->willReturn('some content')
         ;
 
-        $render = $this->actionRenderer->render($action, $data);
 
-        $this->assertEquals('<render></render>', $render);
-    }
+        $render = $this->linkRenderer->render($link);
 
-    public function testRenderWithResourceName(): void
-    {
-        $action = new Link(
-            template: 'my_template.html.twig',
-            resourceName: 'my_resource',
-            operationName: 'my_operation',
-        );
-        $data = new \stdClass();
-
-        $this
-            ->validator
-            ->expects($this->once())
-            ->method('validate')
-            ->with($action, [new Valid()])
-            ->willReturn($this->createMock(ConstraintViolationListInterface::class))
-        ;
-
-        $this
-            ->urlGenerator
-            ->expects($this->once())
-            ->method('generateFromOperationName')
-            ->with('my_resource', 'my_operation', $data)
-            ->willReturn('/my-url')
-        ;
-
-        $this
-            ->environment
-            ->expects($this->once())
-            ->method('render')
-            ->with('my_template.html.twig')
-            ->willReturn('<render></render>')
-        ;
-
-        $render = $this->actionRenderer->render($action, $data);
-
-        $this->assertEquals('<render></render>', $render);
-    }
-
-    public function testInvalidAction(): void
-    {
-        $action = new Link();
-        $data = new \stdClass();
-
-        $violations = $this->createMock(ConstraintViolationListInterface::class);
-
-        $this
-            ->validator
-            ->expects($this->once())
-            ->method('validate')
-            ->with($action, [new Valid()])
-            ->willReturn($violations)
-        ;
-        $violations
-            ->expects($this->once())
-            ->method('count')
-            ->willReturn(1)
-        ;
-
-        $this->expectException(InvalidActionException::class);
-        $this->actionRenderer->render($action, $data);
+        self::assertEquals('some content', $render);
     }
 
     protected function setUp(): void
     {
-        $this->urlGenerator = $this->createMock(UrlGeneratorInterface::class);
-        $this->validator = $this->createMock(ValidatorInterface::class);
-        $this->environment = $this->createMock(Environment::class);
-        $this->actionRenderer = new LinkRenderer(
+        $this->urlGenerator = self::createMock(UrlGeneratorInterface::class);
+        $this->validator = self::createMock(ValidatorInterface::class);
+        $this->environment = self::createMock(Environment::class);
+        $this->linkRenderer = new LinkRenderer(
             $this->urlGenerator,
             $this->validator,
             $this->environment,
