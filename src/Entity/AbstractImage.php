9c8a37ea3b09886533532efdce69e251720911de
johnkrovitch <arnaudfrezet@gmail.com>
2024-08-09 14:00:28 +0200

    feature: add an image interface and remove the image entity to use an abstract class instead

---
+++ src/Entity/AbstractImage.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LAG\AdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\MappedSuperclass]
+abstract class AbstractImage implements ImageInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(unique: true)]
+    private string $uuid;
+
+    protected ?File $file = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $type = null;
+
+    #[ORM\Column]
+    private ?string $path = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $name = null;
+
+    public function __construct()
+    {
+        $this->uuid = Uuid::v4()->toRfc4122();
+    }
+
+    public function __toString(): string
+    {
+        return $this->path ?? '';
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(string $uuid): void
+    {
+        $this->uuid = $uuid;
+    }
+
+    public function getFile(): ?File
+    {
+        return $this->file;
+    }
+
+    public function setFile(?File $file): void
+    {
+        $this->file = $file;
+    }
+
+    public function hasFile(): bool
+    {
+        return $this->file !== null;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): void
+    {
+        $this->type = $type;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(?string $path): void
+    {
+        $this->path = $path;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+}
