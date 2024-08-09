e8c8e28c6920a22d323344844ce292c18d176d42
johnkrovitch <arnaudfrezet@gmail.com>
2024-08-09 14:11:27 +0200

    chore: change namespace in routing after refactoring

--- src/Routing/Route/RouteNameGenerator.php
+++ src/Routing/Route/RouteNameGenerator.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\Routing\Route;
 
-use LAG\AdminBundle\Metadata\AdminResource;
 use LAG\AdminBundle\Resource\Metadata\OperationInterface;
-
+use LAG\AdminBundle\Resource\Metadata\Resource;
 use function Symfony\Component\String\u;
 
 class RouteNameGenerator implements RouteNameGeneratorInterface
 {
-    public function generateRouteName(AdminResource $resource, OperationInterface $operation): string
+    public function generateRouteName(Resource $resource, OperationInterface $operation): string
     {
         return u($resource->getRoutePattern())
-            ->replace('{application}', $resource->getApplicationName())
+            ->replace('{application}', $resource->getApplication())
             ->replace('{resource}', $resource->getName())
             ->replace('{operation}', $operation->getName())
             ->lower()
--- src/Routing/UrlGenerator/PathGenerator.php
+++ src/Routing/UrlGenerator/PathGenerator.php
@@ -5,50 +5,17 @@ declare(strict_types=1);
 namespace LAG\AdminBundle\Routing\UrlGenerator;
 
 use LAG\AdminBundle\Resource\Metadata\OperationInterface;
-use Symfony\Component\String\Inflector\EnglishInflector;
-
 use function Symfony\Component\String\u;
 
-class PathGenerator implements PathGeneratorInterface
+final readonly class PathGenerator implements PathGeneratorInterface
 {
     public function generatePath(OperationInterface $operation): string
     {
-        $resource = $operation->getResource();
-
-        if (!u($resource->getName())->endsWith('s')) {
-            $resourceName = (new EnglishInflector())->pluralize($resource->getName())[0];
-        } else {
-            $resourceName = $resource->getName();
-        }
-        $path = u($resource->getRoutePrefix())
-            ->replace('{resourceName}', $resourceName)
+        return u()
+            ->append($operation->getPath())
+            ->ensureStart('/')
+            ->trimEnd('/')
+            ->toString()
         ;
-
-        if ($operation->getPath()) {
-            $path
-                ->ensureEnd('/')
-                ->append($operation->getPath())
-            ;
-        } else {
-            foreach ($operation->getRouteParameters() as $parameter => $requirement) {
-                $path = $path
-                    ->append('/')
-                    ->append('{'.$parameter.'}')
-                ;
-            }
-        }
-        $operationPath = u($operation->getPath() ?? '');
-
-        if ($operationPath->length() > 0) {
-            $operationPath = $operationPath->ensureStart('/');
-        }
-
-        $path = $path->append($operationPath->toString());
-
-        if ($path->endsWith('/')) {
-            $path = $path->beforeLast('/');
-        }
-
-        return $path->toString();
     }
 }
