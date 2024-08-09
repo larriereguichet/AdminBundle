4165fb91da98023ea25e7818486f7ea956498d2b
johnkrovitch <arnaudfrezet@gmail.com>
2024-08-09 14:16:13 +0200

    chore: remove unused compiler pass

--- src/LAGAdminBundle.php
+++ src/LAGAdminBundle.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace LAG\AdminBundle;
 
 use LAG\AdminBundle\DependencyInjection\CompilerPass\EventCompilerPass;
-use LAG\AdminBundle\DependencyInjection\CompilerPass\SlugMappingCompilerPass;
 use LAG\AdminBundle\DependencyInjection\LAGAdminExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
@@ -16,7 +15,6 @@ final class LAGAdminBundle extends AbstractBundle
     public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new EventCompilerPass());
-        $container->addCompilerPass(new SlugMappingCompilerPass());
     }
 
     public function getPath(): string
