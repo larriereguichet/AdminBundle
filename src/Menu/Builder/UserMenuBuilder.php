2e89fe1f9e99ac0ffe835f65057a30da1347205a
johnkrovitch <arnaudfrezet@gmail.com>
2024-08-09 14:10:49 +0200

    feature: add menu builders to build resources and contextual menus

---
+++ src/Menu/Builder/UserMenuBuilder.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LAG\AdminBundle\Menu\Builder;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+
+class UserMenuBuilder
+{
+    public function __construct(
+        private FactoryInterface $factory,
+    ) {
+    }
+
+    public function build(array $options = []): ItemInterface
+    {
+        $menu = $this->factory->createItem('root', $options);
+        $menu->addChild('lag_admin.security.logout', [
+            'route' => 'lag_admin.logout',
+            'extras' => ['icon' => 'sign-out-alt'],
+        ]);
+
+        return $menu;
+    }
+}
