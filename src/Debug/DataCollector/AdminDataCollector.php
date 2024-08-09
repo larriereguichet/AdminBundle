8eb1738b7b84be05d7cb70132350e36d90f1ebba
johnkrovitch <arnaudfrezet@gmail.com>
2024-08-09 13:59:47 +0200

    chore: improve debug data collection

--- src/Debug/DataCollector/AdminDataCollector.php
+++ src/Debug/DataCollector/AdminDataCollector.php
@@ -4,42 +4,33 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\Debug\DataCollector;
 
-use LAG\AdminBundle\Application\Configuration\ApplicationConfiguration;
-use LAG\AdminBundle\Metadata\Registry\ResourceRegistryInterface;
-use LAG\AdminBundle\Request\Extractor\ParametersExtractorInterface;
+use LAG\AdminBundle\Request\Extractor\ResourceParametersExtractorInterface;
+use LAG\AdminBundle\Resource\Registry\ResourceRegistryInterface;
+use Symfony\Bundle\FrameworkBundle\DataCollector\AbstractDataCollector;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\DataCollector\DataCollector;
 
-class AdminDataCollector extends DataCollector
+final class AdminDataCollector extends AbstractDataCollector
 {
     public function __construct(
-        private ResourceRegistryInterface $registry,
-        private ApplicationConfiguration $applicationConfiguration,
-        private ParametersExtractorInterface $parametersExtractor,
+        private readonly ResourceRegistryInterface $registry,
+        private readonly ResourceParametersExtractorInterface $parametersExtractor,
     ) {
+    }
+
+    public static function getTemplate(): ?string
+    {
+        return '@LAGAdmin/debug/template.html.twig';
     }
 
     public function collect(Request $request, Response $response, \Throwable $exception = null): void
     {
-        $data = [];
-        $data['resources'] = $this->registry->all();
-
-        if ($this->applicationConfiguration->isFrozen()) {
-            $data['application'] = $this->applicationConfiguration->toArray();
-        }
-
-        if ($this->parametersExtractor->supports($request)) {
-            $data['request']['resource'] = $this->parametersExtractor->getResourceName($request);
-            $data['request']['operation'] = $this->parametersExtractor->getOperationName($request);
-        }
+        $data['application'] = $this->parametersExtractor->getApplicationName($request);
+        $data['resource'] = $this->parametersExtractor->getResourceName($request);
+        $data['operation'] = $this->parametersExtractor->getOperationName($request);
+        $data['resources'] = $this->collectResources();
 
         $this->data = $data;
-    }
-
-    public function getName(): string
-    {
-        return self::class;
     }
 
     public function reset(): void
@@ -50,5 +41,16 @@ class AdminDataCollector extends DataCollector
     public function getData(): array
     {
         return $this->data;
+    }
+
+    private function collectResources(): array
+    {
+        $data = [];
+
+        foreach ($this->registry->all() as $resource) {
+            $data[$resource->getName()] = $resource;
+        }
+
+        return $data;
     }
 }
