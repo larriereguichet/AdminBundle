233b2efc0123656b0b76cece3ec36de7190d9d67
johnkrovitch <arnaudfrezet@gmail.com>
2024-09-07 12:30:06 +0200

    feature: add a workflow subject interface to represent workflow related objects

---
+++ src/Workflow/WorkflowSubjectInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LAG\AdminBundle\Workflow;
+
+interface WorkflowSubjectInterface
+{
+    public function getWorkflow(): ?string;
+
+    public function getWorkflowTransition(): ?string;
+}
