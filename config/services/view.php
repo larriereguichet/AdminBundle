f39fd93d6d5e8578a1c77843b97df31ff10b32fe
johnkrovitch <arnaudfrezet@gmail.com>
2024-09-12 18:00:33 +0200

    feat: add a Twig global vars to ease admin data usage in templates

--- config/services/view.php
+++ config/services/view.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use LAG\AdminBundle\Bridge\QuillJs\Render\QuillJsRendererInterface;
 use LAG\AdminBundle\Grid\Render\CellRendererInterface;
 use LAG\AdminBundle\Grid\Render\GridRendererInterface;
 use LAG\AdminBundle\Grid\Render\HeaderRendererInterface;
 use LAG\AdminBundle\Grid\Render\LinkRendererInterface;
 use LAG\AdminBundle\Resource\Context\ResourceContextInterface;
+use LAG\AdminBundle\Resource\Registry\ApplicationRegistryInterface;
 use LAG\AdminBundle\Resource\Registry\ResourceRegistryInterface;
 use LAG\AdminBundle\Routing\UrlGenerator\UrlGeneratorInterface;
 use LAG\AdminBundle\Twig\Extension\AdminExtension;
@@ -16,7 +18,7 @@ use LAG\AdminBundle\Twig\Extension\GridExtension;
 use LAG\AdminBundle\Twig\Extension\PaginationExtension;
 use LAG\AdminBundle\Twig\Extension\RenderExtension;
 use LAG\AdminBundle\Twig\Extension\RoutingExtension;
-use LAG\AdminBundle\Twig\Extension\StringExtension;
+use LAG\AdminBundle\Twig\Extension\TextExtension;
 use LAG\AdminBundle\View\Component\Cell\Actions;
 use LAG\AdminBundle\View\Component\Cell\FormComponent;
 use LAG\AdminBundle\View\Component\Cell\ImageComponent;
@@ -24,6 +26,8 @@ use LAG\AdminBundle\View\Component\Cell\Link;
 use LAG\AdminBundle\View\Component\Cell\MapComponent;
 use LAG\AdminBundle\View\Component\Cell\TextComponent;
 use LAG\AdminBundle\View\Component\Grid\Grid;
+use LAG\AdminBundle\View\Vars\LAGAdminVars;
+use LAG\AdminBundle\View\Vars\LAGAdminVarsInterface;
 use LAG\AdminBundle\View\Helper\GridHelper;
 use LAG\AdminBundle\View\Helper\GridHelperInterface;
 use LAG\AdminBundle\View\Helper\PaginationHelper;
@@ -32,6 +36,8 @@ use LAG\AdminBundle\View\Helper\RenderHelper;
 use LAG\AdminBundle\View\Helper\RenderHelperInterface;
 use LAG\AdminBundle\View\Helper\RoutingHelper;
 use LAG\AdminBundle\View\Helper\RoutingHelperInterface;
+use LAG\AdminBundle\View\Helper\TextHelper;
+use LAG\AdminBundle\View\Helper\TextHelperInterface;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
@@ -48,9 +54,8 @@ return static function (ContainerConfigurator $container): void {
         ->arg('$helper', service(GridHelperInterface::class))
         ->tag('twig.extension')
     ;
-    $services->set(StringExtension::class)
-        ->arg('$translationDomain', param('lag_admin.translation_domain'))
-        ->arg('$translator', service('translator'))
+    $services->set(TextExtension::class)
+        ->arg('$helper', service(TextHelperInterface::class))
         ->tag('twig.extension')
     ;
     $services->set(RoutingExtension::class)
@@ -94,5 +99,17 @@ return static function (ContainerConfigurator $container): void {
         ->arg('$gridRenderer', service(GridRendererInterface::class))
         ->arg('$headerRenderer', service(HeaderRendererInterface::class))
         ->arg('$cellRenderer', service(CellRendererInterface::class))
+    ;
+    $services->set(TextHelperInterface::class, TextHelper::class)
+        ->arg('$quillJsRenderer', service(QuillJsRendererInterface::class))
+    ;
+
+    // Factories
+    $services->set(LAGAdminVarsInterface::class, LAGAdminVars::class)
+        ->arg('$context', service(ResourceContextInterface::class))
+        ->arg('$requestStack', service('request_stack'))
+        ->arg('$applicationRegistry', service(ApplicationRegistryInterface::class))
+
+        ->alias('lag_admin.twig_vars', LAGAdminVarsInterface::class)
     ;
 };
---
+++ src/View/Vars/LAGAdminVarsInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LAG\AdminBundle\View\Vars;
+
+use LAG\AdminBundle\Resource\Metadata\Application;
+use LAG\AdminBundle\Resource\Metadata\OperationInterface;
+use LAG\AdminBundle\Resource\Metadata\Resource;
+
+interface LAGAdminVarsInterface
+{
+    public function getApplication(): ?Application;
+
+    public function getResource(): ?Resource;
+
+    public function getOperation(): ?OperationInterface;
+}
