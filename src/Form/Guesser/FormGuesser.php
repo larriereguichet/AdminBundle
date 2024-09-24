ef693909558338ccc05c83166ffb5a17fe40ea5f
johnkrovitch <arnaudfrezet@gmail.com>
2024-09-24 23:19:45 +0200

    feat: add a form type guesser to handle non-defined forms using the resource and operation properties

---
+++ src/Form/Guesser/FormGuesser.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace LAG\AdminBundle\Form\Guesser;
+
+use LAG\AdminBundle\Form\Type\Text\TextareaType;
+use LAG\AdminBundle\Resource\Metadata\Boolean;
+use LAG\AdminBundle\Resource\Metadata\Collection;
+use LAG\AdminBundle\Resource\Metadata\Date;
+use LAG\AdminBundle\Resource\Metadata\Map;
+use LAG\AdminBundle\Resource\Metadata\OperationInterface;
+use LAG\AdminBundle\Resource\Metadata\PropertyInterface;
+use LAG\AdminBundle\Resource\Metadata\RichText;
+use LAG\AdminBundle\Resource\Metadata\Slug;
+use LAG\AdminBundle\Resource\Metadata\Text;
+use LAG\AdminBundle\Resource\Metadata\Title;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+final readonly class FormGuesser implements FormGuesserInterface
+{
+    public function guessFormType(OperationInterface $operation, PropertyInterface $property): ?string
+    {
+        if (in_array($property->getName(), $operation->getIdentifiers())) {
+            return null;
+        }
+
+        if ($property instanceof Text || $property instanceof Slug || $property instanceof Title) {
+            return TextType::class;
+        }
+
+        if ($property instanceof Boolean) {
+            return CheckboxType::class;
+        }
+
+        if ($property instanceof Collection) {
+            return CollectionType::class;
+        }
+
+        if ($property instanceof Date) {
+            return DateType::class;
+        }
+
+        if ($property instanceof Map) {
+            return ChoiceType::class;
+        }
+
+        if ($property instanceof RichText) {
+            return TextareaType::class;
+        }
+
+        return null;
+    }
+
+    public function guessFormOptions(OperationInterface $operation, PropertyInterface $property): array
+    {
+        $options = ['required' => false];
+
+        if (is_string($property->getPropertyPath()) && $property->getPropertyPath() !== '.') {
+            $options['property_path'] = $property->getPropertyPath();
+        }
+
+        if ($property instanceof Collection) {
+            $options['entry_type'] = $this->guessFormType($operation, $property->getEntryProperty());
+            $options['entry_options'] = $this->guessFormOptions($operation, $property->getEntryProperty());
+        }
+
+        if ($property instanceof Map) {
+            $options['choices'] = array_flip($property->getMap());
+        }
+
+        return $options;
+    }
+}
