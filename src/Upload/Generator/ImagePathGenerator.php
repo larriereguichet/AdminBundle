dc87b086b3ee48298a898993a08e1292da88a248
johnkrovitch <arnaudfrezet@gmail.com>
2024-11-04 09:45:15 +0100

    chore: chore improve image path generation using the native file extension instead of the guessed one

--- src/Upload/Generator/ImagePathGenerator.php
+++ src/Upload/Generator/ImagePathGenerator.php
@@ -6,6 +6,7 @@ namespace LAG\AdminBundle\Upload\Generator;
 
 use LAG\AdminBundle\Entity\ImageInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use function Symfony\Component\String\u;
 
 class ImagePathGenerator implements ImagePathGeneratorInterface
 {
@@ -14,8 +15,9 @@ class ImagePathGenerator implements ImagePathGeneratorInterface
         /** @var UploadedFile $file */
         $file = $image->getFile();
 
+        $extension = u($file->getRealPath())->afterLast('.');
         $hash = bin2hex(random_bytes(16));
-        $path = $hash.'.'.$file->guessExtension();
+        $path = $hash.'.'.$extension;
 
         return \sprintf('%s/%s/%s', substr($path, 0, 2), substr($path, 2, 2), substr($path, 4));
     }
