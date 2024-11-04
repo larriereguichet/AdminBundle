0c51740f99f47d0c73e55cc4e10dfd413b9a865a
johnkrovitch <arnaudfrezet@gmail.com>
2024-11-04 09:46:27 +0100

    chore: rename routing vars in php configuration files

--- config/routing/homepage.php
+++ config/routing/homepage.php
@@ -7,8 +7,8 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use Symfony\Bundle\FrameworkBundle\Controller\TemplateController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
-return static function (RoutingConfigurator $routingConfigurator): void {
-    $routingConfigurator->add('lag_admin.homepage', '/')
+return static function (RoutingConfigurator $routing): void {
+    $routing->add('lag_admin.homepage', '/')
         ->controller(TemplateController::class)
         ->defaults(['template' => '@LAGAdmin/pages/home.html.twig', 'priority' => -255])
     ;
--- config/routing/security.php
+++ config/routing/security.php
@@ -7,10 +7,10 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use LAG\AdminBundle\Controller\Security\Login;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
-return static function (RoutingConfigurator $routingConfigurator): void {
-    $routingConfigurator->add('lag_admin.login', '/login')
+return static function (RoutingConfigurator $routing): void {
+    $routing->add('lag_admin.login', '/login')
         ->controller(Login::class)
     ;
-    $routingConfigurator->add('lag_admin.login_check', '/login-check');
-    $routingConfigurator->add('lag_admin.logout', '/logout');
+    $routing->add('lag_admin.login_check', '/login-check');
+    $routing->add('lag_admin.logout', '/logout');
 };
