e416fa5495331d1f869f01a05e798ea6d0853fdc
johnkrovitch <arnaudfrezet@gmail.com>
2024-11-04 09:39:10 +0100

    chore: remove legacy docker test application

--- docker/.docker/php/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-# Install a Symfony project
-cd /home/docker/www/admin_test
-composer require lag/adminbundle:dev-master
-#cat composer.json
-#echo "lol"
-#composer config repositories.admin-bundle url /home/docker/www/admin-bundle
-#ls /home/docker/www/admin-bundle
-#composer require lag/admin-bundle
