8253c31fd5830d74c3d73a6555acb73b1ba92cd0
johnkrovitch <arnaudfrezet@gmail.com>
2024-11-04 10:31:30 +0100

    chore: add a sleep on docker compose up to let mariadb create the required files in Github actions

--- tests/phpunit/Resource/Resolver/ResourceResolverTest.php
+++ tests/phpunit/Resource/Resolver/ResourceResolverTest.php
@@ -57,7 +57,7 @@ final class ResourceResolverTest extends TestCase
             ->expects(self::once())
             ->method('locateProperties')
             ->willReturnMap([
-                [$bookClass, []]
+                [$bookClass, []],
             ])
         ;
 
