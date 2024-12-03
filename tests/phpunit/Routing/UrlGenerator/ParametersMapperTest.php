dad0a28bdff0dd0400c595be403395145bb61358
johnkrovitch <arnaudfrezet@gmail.com>
2024-12-03 23:23:28 +0100

    feat: allow an array to be passed to the parameters mapper

---
+++ tests/phpunit/Routing/UrlGenerator/ParametersMapperTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LAG\AdminBundle\Tests\Routing\UrlGenerator;
+
+use LAG\AdminBundle\Routing\UrlGenerator\ParametersMapper;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class ParametersMapperTest extends TestCase
+{
+    private ParametersMapper $parametersMapper;
+
+    #[Test]
+    #[DataProvider(methodName: 'parameters')]
+    public function itMapParameters(mixed $data, array $routeParameters, array $expectedParameters): void
+    {
+        $parameters = $this->parametersMapper->map($data, $routeParameters);
+
+        self::assertEquals($expectedParameters, $parameters);
+    }
+
+    public static function parameters(): iterable
+    {
+        $stdClass = new \stdClass();
+
+        yield [$stdClass, [], []];
+        yield [null, ['my_param' => 'my_value'], []];
+        yield [['my_param' => 'my_value'], ['my_param' => null], ['my_param' => 'my_value']];
+    }
+
+    protected function setUp(): void
+    {
+        $this->parametersMapper = new ParametersMapper();
+    }
+}
