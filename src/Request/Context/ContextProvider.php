6ca0d4d44e602ca436f73fca597abb25e1d98aa4
johnkrovitch <arnaudfrezet@gmail.com>
2024-12-03 19:39:25 +0100

    feat: use decoration instead of composition to handle request context providers to allow context override

--- src/Request/Context/ContextProvider.php
+++ src/Request/Context/ContextProvider.php
@@ -7,7 +7,7 @@ namespace LAG\AdminBundle\Request\Context;
 use LAG\AdminBundle\Resource\Metadata\OperationInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-final readonly class OperationContextProvider implements ContextProviderInterface
+final readonly class ContextProvider implements ContextProviderInterface
 {
     public function getContext(OperationInterface $operation, Request $request): array
     {
