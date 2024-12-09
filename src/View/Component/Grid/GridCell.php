8b0ef912aa783df07b9b08a624dfdaaff10fdb6e
johnkrovitch <arnaudfrezet@gmail.com>
2024-12-09 21:51:58 +0100

    fix: fix missing cell attributes when rendering a grid

--- src/View/Component/Grid/GridCell.php
+++ src/View/Component/Grid/GridCell.php
@@ -6,6 +6,7 @@ namespace LAG\AdminBundle\View\Component\Grid;
 
 use LAG\AdminBundle\Grid\View\CellView;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\ComponentAttributes;
 
 #[AsTwigComponent(
     name: 'lag_admin:grid_cell',
@@ -15,4 +16,14 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 final class GridCell
 {
     public CellView $cell;
+    public ComponentAttributes $attributes;
+    public ComponentAttributes $rowAttributes;
+
+    public function mount(
+        CellView $cell,
+        array $rowAttributes = [],
+    ): void {
+        $this->cell = $cell;
+        $this->rowAttributes = new ComponentAttributes($cell->rowAttributes + $rowAttributes);
+    }
 }
