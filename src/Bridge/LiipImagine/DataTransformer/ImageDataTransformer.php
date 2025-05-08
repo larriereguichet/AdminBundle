111045c1d511970a68a8107ca45df3fd57f2b2d1
johnkrovitch <arnaudfrezet@gmail.com>
2025-05-08 13:21:10 +0200

    chore: update the LiipImagine bridge with metadata changes

--- src/Bridge/LiipImagine/DataTransformer/ImageDataTransformer.php
+++ src/Bridge/LiipImagine/DataTransformer/ImageDataTransformer.php
@@ -6,8 +6,8 @@ namespace LAG\AdminBundle\Bridge\LiipImagine\DataTransformer;
 
 use LAG\AdminBundle\Entity\ImageInterface;
 use LAG\AdminBundle\Grid\DataTransformer\DataTransformerInterface;
-use LAG\AdminBundle\Resource\Metadata\Image;
-use LAG\AdminBundle\Resource\Metadata\PropertyInterface;
+use LAG\AdminBundle\Metadata\Image;
+use LAG\AdminBundle\Metadata\PropertyInterface;
 use Liip\ImagineBundle\Templating\LazyFilterRuntime;
 
 final readonly class ImageDataTransformer implements DataTransformerInterface
