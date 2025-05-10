6a77cb53fc4dff7c77dc2fbab2fb173a789892ae
johnkrovitch <arnaudfrezet@gmail.com>
2025-05-10 13:26:39 +0200

    feat: remove the form from the login controller to match Symfony recommendations

--- src/Controller/Security/Login.php
+++ src/Controller/Security/Login.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace LAG\AdminBundle\Controller\Security;
 
-use LAG\AdminBundle\Form\Type\Security\LoginType;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Twig\Environment;
@@ -14,22 +11,19 @@ use Twig\Environment;
 final readonly class Login
 {
     public function __construct(
-        private FormFactoryInterface $formFactory,
         private AuthenticationUtils $authenticationUtils,
         private Environment $environment,
     ) {
     }
 
-    public function __invoke(Request $request): Response
+    public function __invoke(): Response
     {
         $error = $this->authenticationUtils->getLastAuthenticationError();
-
-        $form = $this->formFactory->create(LoginType::class);
-        $form->handleRequest($request);
+        $lastUsername = $this->authenticationUtils->getLastUsername();
 
         return new Response($this->environment->render('@LAGAdmin/security/login.html.twig', [
-            'form' => $form->createView(),
             'error' => $error,
+            'username' => $lastUsername,
         ]));
     }
 }
