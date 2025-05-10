acb5daa64b4719c2e9e0c956e806cccd3290d2e1
johnkrovitch <arnaudfrezet@gmail.com>
2025-05-10 13:17:45 +0200

    feat: add an operation exception for missing template

---
+++ src/Exception/Operation/MissingOperationTemplateException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LAG\AdminBundle\Exception\Operation;
+
+use LAG\AdminBundle\Exception\Exception;
+
+final class MissingOperationTemplateException extends Exception
+{
+}
